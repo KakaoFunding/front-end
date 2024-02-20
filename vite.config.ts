36d6354fee4d491766aa026dedb74469f5f8bfb8
uraflower <82873315+uraflower@users.noreply.github.com>
2024-02-20 21:53:31 +0900

    [build]: 절대 경로 설정 (#33)
    - `tsconfig.json`, `vite.config.js`에 절대 경로 설정을 추가함

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@states', replacement: path.resolve(__dirname, 'src/states') },
+      { find: '@stories', replacement: path.resolve(__dirname, 'src/stories') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@types', replacement: path.resolve(__dirname, 'src/types') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
+  },
 });
