3cfc8f3a1646a936ddcdb5260016aa2c152b9d78
uraflower <82873315+uraflower@users.noreply.github.com>
2024-02-22 17:15:21 +0900

    [refactor]: 불필요한 스타일 삭제 (#18)

--- src/components/SearchResult/index.tsx
+++ src/components/SearchResult/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import styles from './index.module.scss';
-
 const SearchResult: React.FC = () => {
-  return <section className={styles.area_search_result}>{/* 탭 목록, 선택한 탭에 따른 컨텐츠 */}</section>;
+  return <section>{/* 탭 목록, 선택한 탭에 따른 컨텐츠 */}</section>;
 };
 
 export default SearchResult;
