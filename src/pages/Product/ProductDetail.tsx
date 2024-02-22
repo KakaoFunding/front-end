149b8a0df87fb781ca10f20777ba544811aeac4e
chestnut1044 <wbk1620@gmail.com>
2024-02-23 05:14:23 +0900

    [feat]: 상품 디테일 영역 레이아웃 구현 (#7)

--- src/pages/Product/ProductDetail.tsx
+++ src/pages/Product/ProductDetail.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import styles from './ProductDetail.module.scss';
+
 const ProductDetail: React.FC = () => {
-  return <article>상품상세정보</article>;
+  return (
+    <article>
+      <div className={styles.area_main}>{/* 상품 썸네일 */}</div>
+      <div className={styles.area_content}>{/* 상품 디테일 */}</div>
+      <div className={styles.area_bottom}>{/* 이브랜드의 인기 선물 */}</div>
+    </article>
+  );
 };
 
 export default ProductDetail;
