a021f6fedbf0fae14905528754bc32cceebb1443
uraflower <82873315+uraflower@users.noreply.github.com>
2024-02-24 22:48:36 +0900

    [feat]: 상품 썸네일 크기 prop 추가 (#6)

--- src/components/ProductItem/Thumbnail.tsx
+++ src/components/ProductItem/Thumbnail.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 
 import styles from './Thumbnail.module.scss';
@@ -5,11 +6,12 @@ import styles from './Thumbnail.module.scss';
 interface ThumbnailProps {
   src: string;
   alt: string;
+  size: 'small' | 'medium' | 'large';
 }
 
-const Thumbnail: React.FC<ThumbnailProps> = ({ src, alt }) => {
+const Thumbnail: React.FC<ThumbnailProps> = ({ src, alt, size }) => {
   return (
-    <div className={styles.thumb_wrapper}>
+    <div className={clsx(styles.thumb_wrapper, styles[size])}>
       <img className={styles.thumb_img} src={src} alt={alt} />
     </div>
   );
