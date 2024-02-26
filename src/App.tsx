e50ebf1fa2c4d8ce652e724248406aad3104a61a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-02-27 02:16:33 +0900

    [style]: 불필요한 주석 삭제 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from './components';
 
-// import styles from './app.module.scss';
 import './styles/variable.module.scss';
 
 const App = () => (
