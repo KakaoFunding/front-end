d0270bc4c0726aa9b8574e06c097c8e0a5b5e30c
ohgus <123801385+ohgus@users.noreply.github.com>
2024-02-28 18:28:10 +0900

    [feat]: Footer 레이아웃 구현 (#1)

---
+++ src/components/Footer/InnerFooter.tsx
@@ -0,0 +1,11 @@
+import styles from './InnerFooter.module.scss';
+
+const InnerFooter = () => {
+  return (
+    <div className={styles.wrapper_footer}>
+      <span className={styles.logo}>펀딩하기</span>
+    </div>
+  );
+};
+
+export default InnerFooter;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import InnerFooter from './InnerFooter';
+
+import styles from './index.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.area_footer}>
+      <InnerFooter />
+    </footer>
+  );
+};
+
+export default Footer;
