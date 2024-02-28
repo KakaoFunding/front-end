4d1b207c8f2802664af7cde9a020d25a39fe2550
chestnut1044 <wbk1620@gmail.com>
2024-02-28 18:45:54 +0900

    [refactor]: 상품 컴포넌트 레이아웃 변경 (#2)

--- src/pages/Product/DetailMain.tsx
+++ src/pages/Product/DetailMain.tsx
@@ -1,5 +1,26 @@
+import styles from './index.module.scss';
+
 const DetailMain = () => {
-  return <section>main</section>;
+  return (
+    <section className={styles.area_detail_main}>
+      <section className={styles.area_thumb}>
+        <img
+          src="/img/defaultProductImg.png"
+          alt="상품이미지"
+          className={styles.prod_img}
+        />
+        <div className={styles.prod_carousel}>캐러셀</div>
+      </section>
+      <section className={styles.area_info}>
+        <div>타이틀</div>
+        <div>별점</div>
+        <div>가격</div>
+        <div>공유</div>
+        <div>브랜드</div>
+        <div>상품요약정보</div>
+      </section>
+    </section>
+  );
 };
 
 export default DetailMain;
