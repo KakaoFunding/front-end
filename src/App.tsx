32c3b153c237307ca744aa38e2058180d59471f6
ohgus <123801385+ohgus@users.noreply.github.com>
2024-02-29 00:33:36 +0900

    [refactor]: app에 footer 연결 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
+import Footer from 'components/Footer';
 import Header from 'components/Header';
 
 const App = () => (
   <>
     <Header />
     <Outlet />
-    <footer>footer</footer>
+    <Footer />
   </>
 );
 
