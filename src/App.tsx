adadf56c26ea6e7d9a9711b7fc03b9e984161a53
ohgus <123801385+ohgus@users.noreply.github.com>
2024-02-28 19:28:06 +0900

    [refactor]: export 방법 변경에 따른 수정 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from 'components/Header';
+
 const App = () => (
   <>
-    <header>header</header>
+    <Header />
     <Outlet />
     <footer>footer</footer>
   </>
