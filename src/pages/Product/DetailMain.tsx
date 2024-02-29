87ff4e693faacc0eb5f0e416b57a49c17e2a1b7b
chestnut1044 <wbk1620@gmail.com>
2024-02-29 20:35:40 +0900

    [feat]: 상품 기본 정보 화면 구현(#2)

--- src/pages/Product/DetailMain.tsx
+++ src/pages/Product/DetailMain.tsx
@@ -1,23 +1,52 @@
 import styles from './DetailMain.module.scss';
 
+const mockData = {
+  img: 'https://img1.kakaocdn.net/thumb/C320x320@2x.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20200622172424_cb6115d1953d460e80d8dc95e6906afa.jpg',
+  title:
+    '수제 컵케이크 4종 (망고무스,오레오,블루베리,초코무스)/*제주도발송불가*',
+  origin: '원산지 : 상품상세참조',
+  rangking: 1,
+  review: '(210건의 선물후기)',
+  price: '23,900원',
+  brandName: '앨리스데이',
+  brandImg:
+    'https://img1.kakaocdn.net/thumb/C50x50@2x.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fgift_brand%2F20240129113909_7058ef11b7d74b8b9bd643f4f0b1b881.png',
+};
+
+function makeStarToArr(startCnt: number) {
+  return Array.from({ length: 4 }, (_, idx) => idx < startCnt);
+}
+
 const DetailMain = () => {
   return (
-    <section className={styles.area_detail_main}>
-      <section className={styles.area_thumb}>
+    <section className={styles.section_detail_main}>
+      <section className={styles.wrapper_thumb}>
         <img
-          src="/img/defaultProductImg.png"
-          alt="상품이미지"
-          className={styles.prod_img}
+          src={mockData.img}
+          alt={`${mockData.title} 상품이미지`}
+          className={styles.img}
         />
-        <div className={styles.prod_carousel}>캐러셀</div>
+        {/* <div className={styles.wrapper_carousel}>캐러셀</div> */}
       </section>
-      <section className={styles.area_info}>
-        <div>타이틀</div>
-        <div>별점</div>
-        <div>가격</div>
-        <div>공유</div>
-        <div>브랜드</div>
-        <div>상품요약정보</div>
+      <section className={styles.wrapper_info}>
+        <section className={styles.prod_desc}>
+          <div className={styles.title}>{mockData.title}</div>
+          <div className={styles.origin}>{mockData.origin}</div>
+          <div className={styles.price}>{mockData.price}</div>
+        </section>
+        <section className={styles.brand_desc}>
+          <div className={styles.wrapper_brand}>
+            <a href="brand/{brandId}" className={styles.link_brand}>
+              <img
+                className={styles.img_thmb}
+                src={mockData.brandImg}
+                alt={`${mockData.brandName}브랜드 이미지`}
+              />
+              <span className={styles.txt_name}>{mockData.brandName}</span>
+              <span className={styles.ico}>ss</span>
+            </a>
+          </div>
+        </section>
       </section>
     </section>
   );
