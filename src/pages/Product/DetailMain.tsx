923b669994fd02efc9eb9e0254db6a269dac3b05
chestnut1044 <wbk1620@gmail.com>
2024-02-29 23:36:19 +0900

    [refactor]: 목데이터 변경(#2)
    여러개의 이미지를 가지고 있는 상품으로 변경

--- src/pages/Product/DetailMain.tsx
+++ src/pages/Product/DetailMain.tsx
@@ -1,16 +1,18 @@
 import styles from './DetailMain.module.scss';
 
 const mockData = {
-  img: 'https://img1.kakaocdn.net/thumb/C320x320@2x.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20200622172424_cb6115d1953d460e80d8dc95e6906afa.jpg',
-  title:
-    '수제 컵케이크 4종 (망고무스,오레오,블루베리,초코무스)/*제주도발송불가*',
-  origin: '원산지 : 상품상세참조',
-  rangking: 1,
-  review: '(210건의 선물후기)',
-  price: '23,900원',
-  brandName: '앨리스데이',
+  img: [
+    'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20221126161239_5038b9a02a4545918505695f3aa5061d.jpg',
+    'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20220111185048_bb1322bf174446aa90ebb1d1d7899483.jpg',
+    'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20220111185043_ff1399dbd788457882ff5e3c42e018e9.jpg',
+    'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20221126161243_61f7ee78cb87485f8af55f5683222dee.jpg',
+  ],
+  title: 'NEW 루쥬 코코 밤(+샤넬 기프트 카드)',
+  origin: '원산지 : 상품상세참조' && '',
+  price: '51,000원',
+  brandName: '샤넬',
   brandImg:
-    'https://img1.kakaocdn.net/thumb/C50x50@2x.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fgift_brand%2F20240129113909_7058ef11b7d74b8b9bd643f4f0b1b881.png',
+    'https://img1.kakaocdn.net/thumb/C200x200@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fgift_brand%2F20200612122729_0c5dad5805cb4aa4855ac7ee831d2e3f.jpg',
 };
 
 const DetailMain = () => {
@@ -18,11 +20,11 @@ const DetailMain = () => {
     <section className={styles.section_detail_main}>
       <section className={styles.wrapper_thumb}>
         <img
-          src={mockData.img}
+          src={mockData.img[0]}
           alt={`${mockData.title} 상품이미지`}
           className={styles.img}
         />
-        {/* <div className={styles.wrapper_carousel}>캐러셀</div> */}
+        <div className={styles.wrapper_carousel}>캐러셀</div>
       </section>
       <section className={styles.wrapper_info}>
         <section className={styles.prod_desc}>
