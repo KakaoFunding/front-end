814f3bfca79e260983262c46aeb929bfa7ce5b37
chestnut1044 <wbk1620@gmail.com>
2024-02-29 19:00:04 +0900

    [feat]: mainwrapper 적용(#2)

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,3 +1,5 @@
+import MainWrapper from 'components/MainWrapper';
+
 import DetailBottom from './DetailBottom';
 import DetailContents from './DetailContents';
 import DetailMain from './DetailMain';
@@ -7,16 +9,18 @@ import styles from './index.module.scss';
 
 const Product = () => {
   return (
-    <article className={styles.area_product}>
-      <section className={styles.area_detail_info}>
-        <DetailMain />
-        <DetailContents />
-        <DetailBottom />
-      </section>
-      <section className={styles.area_buy_info}>
-        <ProductBuyInfo />
-      </section>
-    </article>
+    <MainWrapper>
+      <article className={styles.area_product}>
+        <section className={styles.area_detail_info}>
+          <DetailMain />
+          <DetailContents />
+          <DetailBottom />
+        </section>
+        <section className={styles.area_buy_info}>
+          <ProductBuyInfo />
+        </section>
+      </article>
+    </MainWrapper>
   );
 };
 
