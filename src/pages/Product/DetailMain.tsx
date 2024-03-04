436fba51951a7f4c369820b865880c65652fee53
chestnut1044 <wbk1620@gmail.com>
2024-03-04 13:10:35 +0900

    [refactor]: formatNumberWithComma 함수 적용(#2)

--- src/pages/Product/DetailMain.tsx
+++ src/pages/Product/DetailMain.tsx
@@ -1,3 +1,5 @@
+import { formatNumberWithComma } from '../../utils/format';
+
 import ProductDetailsCarousel from './ProductDetailsCarousel';
 
 import styles from './DetailMain.module.scss';
@@ -11,7 +13,7 @@ const mockData = {
   ],
   title: 'NEW 루쥬 코코 밤(+샤넬 기프트 카드)',
   origin: '원산지 : 상품상세참조' && '',
-  price: '51,000원',
+  price: 51000,
   brandName: '샤넬',
   brandImg:
     'https://img1.kakaocdn.net/thumb/C200x200@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fgift_brand%2F20200612122729_0c5dad5805cb4aa4855ac7ee831d2e3f.jpg',
@@ -27,7 +29,9 @@ const DetailMain = () => {
         <section className={styles.prod_desc}>
           <div className={styles.title}>{mockData.title}</div>
           <div className={styles.origin}>{mockData.origin}</div>
-          <div className={styles.price}>{mockData.price}</div>
+          <div
+            className={styles.price}
+          >{`${formatNumberWithComma(mockData.price)}원`}</div>
         </section>
         <section className={styles.brand_desc}>
           <div className={styles.wrapper_brand}>
