00771b176da1ef25f33944a620ef268da016b395
chestnut1044 <wbk1620@gmail.com>
2024-03-06 22:42:56 +0900

    [feat]: 타이틀/더보기 구현(#11)

--- src/pages/Product/DetailBottom.tsx
+++ src/pages/Product/DetailBottom.tsx
@@ -1,5 +1,23 @@
-const DetailBottom = () => {
-  return <section>bottom</section>;
+import { Link } from 'react-router-dom';
+
+import styles from './DetailBottom.module.scss';
+
+type DetailBottomProps = {
+  brandId: number;
+};
+
+const DetailBottom = ({ brandId }: DetailBottomProps) => {
+  return (
+    <section className={styles.section_detail_bottom}>
+      <div className={styles.wrapper_title}>
+        <div className={styles.txt}>이 브랜드의 인기선물</div>
+        <Link to={`../brand/${brandId}`} className={styles.button}>
+          더보기<span className={styles.ico}>아이콘</span>
+        </Link>
+      </div>
+      <div>리스트</div>
+    </section>
+  );
 };
 
 export default DetailBottom;
