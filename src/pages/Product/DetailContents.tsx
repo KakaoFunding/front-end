8d1d59c955bbc7629184d7174571cd5af79cdea0
chestnut1044 <wbk1620@gmail.com>
2024-03-06 21:30:19 +0900

    [feat]: 상세정보 탭의 상품고시정보 테이블 구현(#5)

--- src/pages/Product/DetailContents.tsx
+++ src/pages/Product/DetailContents.tsx
@@ -2,6 +2,8 @@ import Tabs from 'components/Tabs';
 
 import { Tab } from 'types/tab';
 
+import DetailContentDropDown from './DetailContentDropDown';
+
 import styles from './DetailContents.module.scss';
 
 const productDescriptionImgs = [
@@ -79,8 +81,45 @@ const SectionGiftReview = (
   <section className={styles.wrapper_gift_review}>준비중인 기능입니다.</section>
 );
 
+const mockTableData = [
+  {
+    idx: 1,
+    title: '내용물의 용량 또는 중량',
+    contents: '3.0g',
+  },
+  { idx: 2, title: '제품 주요 사양', contents: '모든 피부 타입' },
+  {
+    idx: 3,
+    title: '사용기한 또는 개봉 후 사용기간',
+    contents:
+      '배송되는 제품은 대부분 상품 발송일로부터 12개월 이상 남아있는 제품부터 순차적으로 배송됩니다.',
+  },
+];
 const SectionDetailInfo = (
-  <section className={styles.wrapper_detail_info}>준비중인 기능입니다.</section>
+  <section className={styles.wrapper_detail_info}>
+    <div className={styles.title}>상품고시정보</div>
+    <div className={styles.description}>
+      <tbody className={styles.table}>
+        {mockTableData.map((tr) => (
+          <tr key={tr.idx} className={styles.wrapper_tr}>
+            <td className={styles.table_title}>{tr.title}</td>
+            <td className={styles.table_contents}>{tr.contents}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
+
+    <DetailContentDropDown
+      title="교환/반품/환불 안내"
+      description="교환/반품/환불/안내 본문"
+      className={{ title: styles.title, description: styles.description }}
+    />
+    <DetailContentDropDown
+      title="구매 시 주의 사항"
+      description="구매 시 주의 사항 본문"
+      className={{ title: styles.title, description: styles.description }}
+    />
+  </section>
 );
 
 const tabProps: Tab[] = [
@@ -104,7 +143,7 @@ const tabProps: Tab[] = [
 const DetailContents = () => {
   return (
     <section className={styles.section_detail_contents}>
-      <Tabs initialTabId={0} tabs={tabProps} mode="product_datail" />
+      <Tabs initialTabId={2} tabs={tabProps} mode="product_datail" />
     </section>
   );
 };
