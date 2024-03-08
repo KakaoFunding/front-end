92f812d2597b63feaa88e4e1bf78aec0a6d00297
chestnut1044 <wbk1620@gmail.com>
2024-03-08 20:49:27 +0900

    [feat]: 상품 아이템 컴포넌트 구현(#11)

---
+++ src/pages/Product/ProductItem.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+
+import CartButton from 'components/ColumnProductItem/CartButton';
+import Price from 'components/ColumnProductItem/Price';
+import Thumbnail from 'components/ColumnProductItem/Thumbnail';
+
+import styles from './ProductItem.module.scss';
+
+type ProductItemType = {
+  product: {
+    id: number;
+    thumbSrc: string;
+    brandName: string;
+    name: string;
+    price: number;
+    isWished: boolean;
+    wishCount: number;
+  };
+};
+
+const ProductItem = ({ product }: ProductItemType) => {
+  return (
+    <article className={clsx(styles.wrapper_prod_item)}>
+      <a href={`/product/${product.id}`} className={styles.wrapper_main_info}>
+        <Thumbnail src={product.thumbSrc} alt={product.name} size="small" />
+        <strong className={styles.txt_prod_name}>{product.name}</strong>
+        <Price price={product.price} />
+      </a>
+      <div className={styles.wrapper_util_info}>
+        <CartButton id={product.id} />
+      </div>
+    </article>
+  );
+};
+
+export default ProductItem;
