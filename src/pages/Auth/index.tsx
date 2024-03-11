58893f1dc60f227dbed7c000a338ca3a6e478477
ohgus <123801385+ohgus@users.noreply.github.com>
2024-03-12 01:37:57 +0900

    [feat]: 로그인 완료 처리 로직 추가 (#32)

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,9 +1,34 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuthStore, useUserStore } from 'store/index';
+
+import { login } from 'services/api/v1/oauth';
 
 const Auth = () => {
+  const navigate = useNavigate();
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const setUserInfo = useUserStore((state) => state.setUserInfo);
+
   useEffect(() => {
     const urlString = new URL(window.location.href);
     const code = urlString.searchParams.get('code');
+    const loginState = urlString.searchParams.get('state');
+
+    if (!code || !loginState) {
+      // Todo: 예외처리
+      navigate('/');
+      return;
+    }
+
+    const result = login({ code });
+
+    result.then((res) => {
+      const { accessToken, member } = res.data;
+      setAccessToken(accessToken);
+      setUserInfo(member);
+      navigate(loginState);
+    });
   });
 
   return <div>로그인중 입니다...</div>;
