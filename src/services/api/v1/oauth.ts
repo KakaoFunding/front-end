359c647654e8c083f5619fcf647b67e83f3b578a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-03-12 01:32:23 +0900

    [feat]: login, logout 가상 api 구현 (#32)

--- src/services/api/v1/oauth.ts
+++ src/services/api/v1/oauth.ts
@@ -1,4 +1,8 @@
-import { AxiosResponse } from 'axios';
+import {
+  AxiosRequestHeaders,
+  AxiosResponse,
+  RawAxiosResponseHeaders,
+} from 'axios';
 
 import { apiV1 } from '.';
 
@@ -7,17 +11,56 @@ interface LoginRequestProps {
 }
 
 interface LoginResponseProps {
-  accessToken: 'string';
+  accessToken: string;
   member: {
-    profileUrl: 'string' | null;
-    name: 'string' | null;
+    name: string;
+    profileUrl: string;
   };
 }
 
 export const login = async ({
   code,
 }: LoginRequestProps): Promise<AxiosResponse<LoginResponseProps>> => {
-  return apiV1.post('/oauth/login', null, {
-    params: { provider: 'kakao', code },
+  return new Promise((res) => {
+    const fakeApiResponse: AxiosResponse<LoginResponseProps> = {
+      data: {
+        accessToken: 'fakeAccessToken',
+        member: {
+          name: 'fake guy',
+          profileUrl: 'fakeUrl',
+        },
+      },
+      status: 200,
+      statusText: 'OK',
+      headers: {} as RawAxiosResponseHeaders,
+      config: { headers: {} as AxiosRequestHeaders },
+    };
+
+    setTimeout(() => res(fakeApiResponse), 200);
   });
+  // apiV1.post('/oauth/login', null, {
+  //   params: { provider: 'kakao', code },
+  // });
+};
+
+export const logout = async (): Promise<AxiosResponse<LoginResponseProps>> => {
+  return new Promise((res) => {
+    const fakeApiResponse: AxiosResponse<LoginResponseProps> = {
+      data: {
+        accessToken: 'fakeAccessToken',
+        member: {
+          name: 'fake guy',
+          profileUrl: 'fakeUrl',
+        },
+      },
+      status: 200,
+      statusText: 'OK',
+      headers: {} as Record<string, string>,
+      config: { headers: {} as AxiosRequestHeaders },
+    };
+
+    setTimeout(() => res(fakeApiResponse), 200);
+  });
+  // const response = await apiV1.get('/oauth/logout');
+  // return response;
 };
