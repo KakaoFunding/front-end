e283d5424aa0acfe318b648c8e24887ececd009f
chestnut1044 <wbk1620@gmail.com>
2024-03-14 02:43:37 +0900

    [feat]: 상품 옵션 선택 기능 구현(#8)

--- src/pages/Product/ProductBuyInfo.tsx
+++ src/pages/Product/ProductBuyInfo.tsx
@@ -1,5 +1,99 @@
+import { useState } from 'react';
+
+import ProductOption from './ProductOption';
+import ProductQuantity from './ProductQuantity';
+
+import styles from './ProductBuyInfo.module.scss';
+
+// 받아올 데이터
+const mockData = {
+  isOption: true,
+  productTitle: '940 코쿤 [New & Limited]',
+  optionTitle: '색상',
+  options: [
+    {
+      id: 1,
+      name: '940 코쿤 [New & Limited]',
+    },
+    {
+      id: 2,
+      name: '934 코랄린 [New & Limited]',
+    },
+    {
+      id: 3,
+      name: '932 아네모네 [New & Limited]',
+    },
+    {
+      id: 4,
+      name: '918 마이 로즈',
+    },
+    {
+      id: 5,
+      name: '916 플러티 코랄',
+    },
+    {
+      id: 6,
+      name: '920 인 러브',
+    },
+    {
+      id: 7,
+      name: '912 드리미 화이트',
+    },
+    {
+      id: 8,
+      name: '914 내츄럴 잠',
+    },
+    {
+      id: 9,
+      name: '922 패션 핑크',
+    },
+    {
+      id: 10,
+      name: '924 폴 포 미',
+    },
+    {
+      id: 11,
+      name: '928 핑크 딜라이트',
+    },
+    {
+      id: 12,
+      name: '930 스윗 트릿',
+    },
+    {
+      id: 13,
+      name: '936 칠링 핑크 [New]',
+    },
+    {
+      id: 14,
+      name: '938 킵 쿨 [New]',
+    },
+  ],
+};
+
+// 수량 + 가격 계산 컴포넌트
 const ProductBuyInfo = () => {
-  return <section>구매정보</section>;
+  const [quantity, setQuantity] = useState<number>(1);
+
+  return (
+    <section className={styles.area_buy_info}>
+      {mockData.isOption ? (
+        <ProductOption
+          optionTitle={mockData.optionTitle}
+          options={mockData.options}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
+      ) : (
+        <ProductQuantity
+          hasOption={false}
+          optionName={mockData.productTitle}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
+      )}
+      {/* 계산 컴포넌트 */}
+    </section>
+  );
 };
 
 export default ProductBuyInfo;
---
+++ src/pages/Product/ProductOption.tsx
@@ -0,0 +1,91 @@
+import clsx from 'clsx';
+import { useEffect, useState, Dispatch, SetStateAction } from 'react';
+
+import ProductQuantity from './ProductQuantity';
+
+import styles from './ProductOption.module.scss';
+
+// 나중에 타입정의해서 types 폴더로 갈 예정
+type Option = {
+  id: number;
+  name: string;
+};
+
+type ProductOptionProps = {
+  optionTitle: string;
+  options: Option[];
+  quantity: number;
+  setQuantity: Dispatch<SetStateAction<number>>;
+};
+
+const ProductOption = ({
+  optionTitle,
+  options,
+  quantity,
+  setQuantity,
+}: ProductOptionProps) => {
+  const [isToggled, setIsToggled] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<
+    false | { id: number; name: string }
+  >(false);
+
+  useEffect(() => {
+    if (selectedOption) {
+      setIsToggled(true);
+    } else {
+      setIsToggled(false);
+    }
+  }, [selectedOption]);
+
+  const handleToggle = () => {
+    if (!selectedOption) {
+      return;
+    }
+    setIsToggled(!isToggled);
+  };
+
+  const handleOptionChange = (option: { id: number; name: string }) =>
+    setSelectedOption(option);
+
+  const handleOptionClear = () => setSelectedOption(false);
+
+  return (
+    <section className={styles.area_option}>
+      <div className={clsx(styles.default, styles.notice)}>
+        선물 받은 친구가 직접 옵션 변경 가능하니 안심하세요!
+      </div>
+      <div className={clsx(styles.default, styles.txt_option)}>
+        {selectedOption ? selectedOption.name : optionTitle}
+        <button type="button" onClick={handleToggle} aria-label="toggle button">
+          <span
+            className={clsx(styles.ico_toggle, { [styles.on]: isToggled })}
+          />
+        </button>
+      </div>
+      <ul className={clsx({ [styles.wrapper_option]: isToggled })}>
+        {options.map((option) => (
+          <li key={option.id} className={styles.default}>
+            {option.name}
+            <input
+              type="radio"
+              name="option"
+              checked={selectedOption && selectedOption.id === option.id}
+              onChange={() => handleOptionChange(option)}
+            />
+          </li>
+        ))}
+      </ul>
+      {selectedOption && (
+        <ProductQuantity
+          hasOption
+          optionName={selectedOption.name}
+          handleOptionClear={handleOptionClear}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
+      )}
+    </section>
+  );
+};
+
+export default ProductOption;
