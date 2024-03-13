4722e961f09c55c12ad7915572b381a22928d76f
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-13 21:40:05 +0900

    [fix]: eslint 리턴 에러 끄기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import Product from 'pages/Product';
 
 import App from './App';
 
+// eslint-disable-next-line consistent-return
 const enableMocking = async () => {
   if (process.env.NODE_ENV === 'development') {
     const { worker } = await import('mocks/worker');
