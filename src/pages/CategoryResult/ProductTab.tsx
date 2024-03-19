05a56aed347f404c0d1979f921a6f3e3c1d9e2a3
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-19 17:11:53 +0900

    [feat]: 상품 목록 무한 스크롤 구현 (#95)

---
+++ src/pages/CategoryResult/ProductTab.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+
+import ProductList from 'components/ProductList';
+
+import { useAxios } from 'hooks/useAxios';
+
+import { PaginationResponse } from 'types/PaginationResponse';
+import { Category } from 'types/category';
+import { ProductItem } from 'types/productItem';
+
+type ProductTabProps = {
+  categoryId: Category['categoryId'];
+};
+
+const ProductTab = ({ categoryId }: ProductTabProps) => {
+  const [page, setPage] = useState<number>(0);
+  const [hasNext, setHasNext] = useState<boolean>(true);
+  const [products, setProducts] = useState<ProductItem[]>([]);
+
+  const { data, isLoading, sendRequest } = useAxios<
+    PaginationResponse<ProductItem>
+  >({
+    method: 'get',
+    url: `/products`,
+    params: {
+      page,
+      size: 20,
+      categoryId,
+    },
+  });
+
+  useEffect(() => {
+    sendRequest();
+  }, [page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      if (window.innerHeight + scrollTop >= offsetHeight && hasNext) {
+        setPage((prev) => prev + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      setProducts((prev) => [...prev, ...data.content]);
+      setHasNext(!data.last);
+    }
+  }, [data]);
+
+  return (
+    <>
+      {/* 탭 이름(제목) + 아이템 개수(필터링에 따라 달라질 수 있음) + 정렬 드롭다운 */}
+      {/* 가격 필터링 */}
+      <ProductList products={products} />
+      {isLoading && (
+        <div style={{ backgroundColor: 'red', height: 500 }}>로딩중...</div>
+      )}
+    </>
+  );
+};
+
+export default ProductTab;
