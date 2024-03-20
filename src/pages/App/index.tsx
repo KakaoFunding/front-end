c6b9c62bcb92712b703ba4dece06ab0329942e49
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-20 23:18:52 +0900

    [style]: import 순서 적용 (#82)

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,6 +1,7 @@
+import { Outlet } from 'react-router-dom';
+
 import Footer from 'layouts/App/Footer';
 import Header from 'layouts/App/Header';
-import { Outlet } from 'react-router-dom';
 
 import styles from './index.module.scss';
 
