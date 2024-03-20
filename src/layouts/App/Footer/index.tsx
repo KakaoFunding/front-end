ae6631e4f93715e9694da902dee01ae8e4d7ad70
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-20 23:17:39 +0900

    [refactor]: InnderFooter를 Footer로 병합 (#82)

--- src/layouts/App/Footer/index.tsx
+++ src/layouts/App/Footer/index.tsx
@@ -1,11 +1,11 @@
-import InnerFooter from './InnerFooter';
-
 import styles from './index.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.area_footer}>
-      <InnerFooter />
+      <div className={styles.wrapper_footer}>
+        <span className={styles.logo}>펀딩하기</span>
+      </div>
     </footer>
   );
 };
