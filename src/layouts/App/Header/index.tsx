333151d78daf6b9b5754a5d1353478e7fad17a36
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-20 23:16:06 +0900

    [refactor]: InnderHeader를 Header로 병합 (#82)

--- src/layouts/App/Header/index.tsx
+++ src/layouts/App/Header/index.tsx
@@ -1,11 +1,19 @@
-import InnerHeader from './InnerHeader';
+import BundleUtil from './BundleUtil';
+import CategoryButton from './CategoryButton';
+import HeaderNav from './HeaderNav';
+import Logo from './Logo/Logo';
 
 import styles from './index.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.area_header}>
-      <InnerHeader />
+      <section className={styles.wrapper_header}>
+        <Logo />
+        <HeaderNav />
+        <CategoryButton />
+        <BundleUtil />
+      </section>
     </header>
   );
 };
