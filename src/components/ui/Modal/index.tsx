3c836424bf16d902f55fbba2091e3c2fe954a329
chestnut1044 <wbk1620@gmail.com>
2024-03-27 22:54:39 +0900

    [feat]: 모달 컴포넌트 구현(#126)

---
+++ src/components/ui/Modal/index.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+import styles from './index.module.scss';
+
+type ModalProps = {
+  onClose: () => void;
+  children: ReactNode;
+  className: string;
+};
+
+const Modal = ({ onClose, children, className }: ModalProps) => {
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div className={styles.wrapper_modal} onClick={onClose}>
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      <div
+        className={clsx(styles.modal, className)}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+        <button type="button" className={styles.btn_close} onClick={onClose}>
+          <span className={styles.ico_close}>닫기</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
