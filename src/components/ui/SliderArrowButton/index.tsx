695cdf831957711bec95c7f5f31975c4bfa1afe8
uraflower <82873315+uraflower@users.noreply.github.com>
2024-03-27 16:09:20 +0900

    [feat]: 캐러셀 화살표 버튼 disabled 속성 추가 (#15)

--- src/components/ui/SliderArrowButton/index.tsx
+++ src/components/ui/SliderArrowButton/index.tsx
@@ -4,15 +4,21 @@ import styles from './index.module.scss';
 
 type SliderArrowButtonProps = {
   arrowType: 'prev' | 'next';
+  className?: string | undefined;
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 };
 
-const SliderArrowButton = ({ arrowType, onClick }: SliderArrowButtonProps) => {
+const SliderArrowButton = ({
+  arrowType,
+  className,
+  onClick,
+}: SliderArrowButtonProps) => {
   return (
     <button
       type="button"
       className={clsx(styles.btn_arrow, styles[`btn_${arrowType}`])}
       onClick={onClick}
+      disabled={className?.includes('slick-disabled')}
     >
       <span className={styles[`ico_${arrowType}`]}>
         {arrowType === 'prev' ? '이전' : '다음'}
