33729bd837da45b7a590896a30e8aef24abfce64
chestnut1044 <wbk1620@gmail.com>
2024-03-28 19:14:59 +0900

    [refactor]: Modal 컴포넌트 조건부 렌더링(#126)

--- src/components/ui/Modal/index.tsx
+++ src/components/ui/Modal/index.tsx
@@ -1,24 +1,28 @@
 import clsx from 'clsx';
+import { createPortal } from 'react-dom';
 
 import { ModalProps } from 'types/modal';
 
 import styles from './index.module.scss';
 
-const Modal = ({ onClose, children, className }: ModalProps) => {
-  return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div className={styles.wrapper_modal} onClick={onClose}>
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-      <div
-        className={clsx(styles.modal, className)}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {children}
-        <button type="button" className={styles.btn_close} onClick={onClose}>
-          <span className={styles.ico_close}>닫기</span>
-        </button>
+const Modal = ({ onClose, children, className, isOpen }: ModalProps) => {
+  return createPortal(
+    isOpen && (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+      <div className={styles.wrapper_modal} onClick={onClose}>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+        <div
+          className={clsx(styles.modal, className)}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+          <button type="button" className={styles.btn_close} onClick={onClose}>
+            <span className={styles.ico_close}>닫기</span>
+          </button>
+        </div>
       </div>
-    </div>
+    ),
+    document.getElementById('modal') as HTMLElement,
   );
 };
 
