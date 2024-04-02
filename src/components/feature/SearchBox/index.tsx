8984e47dd528ba6755fd4c9c87bdacd98fc0e027
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-02 13:28:29 +0900

    [feat]: 검색창 UI 구현 (#135)

--- src/components/feature/SearchBox/index.tsx
+++ src/components/feature/SearchBox/index.tsx
@@ -1,5 +1,21 @@
+import styles from './index.module.scss';
+
 const SearchBox = () => {
-  return <div>검색창</div>;
+  return (
+    <div className={styles.area_search}>
+      <div className={styles.wrapper_input}>
+        <form role="search">
+          <input
+            type="search"
+            aria-label="검색어 입력"
+            placeholder="검색어를 입력해주세요."
+            autoComplete="off"
+            className={styles.input}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBox;
