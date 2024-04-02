68393046798fd9f93deaac9622ea7082e60f01fa
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-02 18:33:57 +0900

    [feat]: 검색 결과 페이지 탭 구현 (#140)

--- src/layouts/SearchResult/SearchContents/index.tsx
+++ src/layouts/SearchResult/SearchContents/index.tsx
@@ -1,5 +1,15 @@
+import Tabs from 'components/ui/Tabs';
+
+import { Tab } from 'types/tab';
+
 const SearchContents = () => {
-  return <div>검색 결과</div>;
+  const tabs: Tab[] = [
+    { id: 0, name: '전체', content: '' },
+    { id: 1, name: '상품', content: '' },
+    { id: 2, name: '브랜드', content: '' },
+  ];
+
+  return <Tabs initialTabId={0} tabs={tabs} mode="product_list" />;
 };
 
 export default SearchContents;
