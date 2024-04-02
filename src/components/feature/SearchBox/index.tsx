0f249354cb53f47b908b53fe3247385906c42906
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-02 13:53:50 +0900

    [feat]: useInput 커스텀 훅 구현 (#135)

--- src/components/feature/SearchBox/index.tsx
+++ src/components/feature/SearchBox/index.tsx
@@ -1,6 +1,10 @@
+import { useInput } from 'hooks/useInput';
+
 import styles from './index.module.scss';
 
 const SearchBox = () => {
+  const { value, handleInput, handleClear } = useInput('');
+
   return (
     <div className={styles.area_search}>
       <div className={styles.wrapper_input}>
@@ -10,8 +14,15 @@ const SearchBox = () => {
             aria-label="검색어 입력"
             placeholder="검색어를 입력해주세요."
             autoComplete="off"
+            value={value}
+            onChange={handleInput}
             className={styles.input}
           />
+          {value && (
+            <button type="button" onClick={handleClear}>
+              clear
+            </button>
+          )}
         </form>
       </div>
     </div>
