b53eb2a3bb48e305dd5212020a43d4108fea1ed0
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-02 15:46:49 +0900

    [feat]: 검색 버튼 구현 (#135)

--- src/components/feature/SearchBox/index.tsx
+++ src/components/feature/SearchBox/index.tsx
@@ -1,14 +1,22 @@
+import { useLocation } from 'react-router-dom';
+
 import { useInput } from 'hooks/useInput';
 
 import styles from './index.module.scss';
 
 const SearchBox = () => {
   const { value, handleInput, handleClear } = useInput('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate(`/search/result?keyword=${value}`);
+  };
 
   return (
     <div className={styles.area_search}>
       <div className={styles.wrapper_input}>
-        <form role="search">
+        <form role="search" onSubmit={handleSubmit}>
           <input
             type="search"
             aria-label="검색어 입력"
@@ -23,6 +31,7 @@ const SearchBox = () => {
               clear
             </button>
           )}
+          {value && <button type="submit">확인</button>}
         </form>
       </div>
     </div>
