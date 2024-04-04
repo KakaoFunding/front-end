49bd34a1a9fd5743de086979203b0b70c413326c
chestnut1044 <wbk1620@gmail.com>
2024-04-04 17:28:10 +0900

    [feat]: Receiver 컴포넌트 프로필 레이아웃 구현(#123)
    - 파일명 변경으로 병합된 파일 삭제
    - Receiver 레이아웃 구현

--- src/layouts/Home/Receiver/index.tsx
+++ src/layouts/Home/Receiver/index.tsx
@@ -1,5 +1,25 @@
+import ProfileImg from 'components/feature/ProfileImg';
+import MainWrapper from 'components/ui/MainWrapper';
+
+import styles from './index.module.scss';
+
 const Receiver = () => {
-  return <>친구선택</>;
+  return (
+    <section>
+      <div className={styles.wrapper_selector}>
+        <ProfileImg
+          size="l"
+          hasIcon="plus"
+          onClick={() => console.log('친구 선택')}
+        />
+        나를 위한 선물하기
+      </div>
+      <MainWrapper>
+        <div>판딩</div>
+        <div>위시</div>
+      </MainWrapper>
+    </section>
+  );
 };
 
 export default Receiver;
