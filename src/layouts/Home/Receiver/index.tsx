3a2f8559e36f96d2c09257d1cac873f0062d037a
chestnut1044 <wbk1620@gmail.com>
2024-04-07 23:04:48 +0900

    [feat]: 친구선택 영역 구현(#123)
    - 선택된 친구 상태와 로그인 여부에 따라 프로필 이미지와 타이틀이 달라지도록 구현

--- src/layouts/Home/Receiver/index.tsx
+++ src/layouts/Home/Receiver/index.tsx
@@ -1,21 +1,57 @@
 import ProfileImg from 'components/feature/ProfileImg';
 import MainWrapper from 'components/ui/MainWrapper';
 
+import { useSelectedFriendsStore } from 'store/useSelectedFriendsStore';
+
 import styles from './index.module.scss';
 
+const mockdata = {
+  login: false,
+  name: '보경',
+  myProfileImgUrl: '',
+};
+
 const Receiver = () => {
+  const {
+    isSelected,
+    isSelfSelected,
+    getImgUrl,
+    getTitle,
+    setSelectedFriends,
+  } = useSelectedFriendsStore();
+
+  // TODO : 개발용, 배포시 삭제
+  // useSelectedFriendsStore.persist.clearStorage();
+
+  // 임시 친구 선택
+  const handleSelectedFriends = () =>
+    setSelectedFriends([...prompt()!.toString()]);
+
   return (
     <section>
       <div className={styles.wrapper_selector}>
         <ProfileImg
           size="l"
+          imgUrl={
+            mockdata.login && isSelfSelected
+              ? mockdata.myProfileImgUrl
+              : getImgUrl()
+          }
           hasIcon="plus"
-          onClick={() => console.log('친구 선택')}
+          onClick={handleSelectedFriends}
         />
-        나를 위한 선물하기
+        <strong className={styles.title_selector}>
+          {mockdata.login && !isSelected && (
+            <>
+              {`${mockdata.name}님`}
+              <br />
+            </>
+          )}
+          {getTitle()}
+        </strong>
       </div>
       <MainWrapper>
-        <div>판딩</div>
+        <div>펀딩</div>
         <div>위시</div>
       </MainWrapper>
     </section>
--- src/store/useSelectedFriendsStore.ts
+++ src/store/useSelectedFriendsStore.ts
@@ -1,19 +1,62 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create, StateCreator } from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
 
-export const useSelectedFriendsStore = create(
-  persist(
-    (set) => ({
+// import { User } from 'types/user';
+
+const defaultImgUrl = 'src/assets/bg_profile_default.png';
+const peopleImgUrl = 'src/assets/profile_people.png';
+const profileImgUrl = 'src/assets/profile.png';
+
+type SelectedFriendsState = {
+  isSelected: boolean;
+  isSelfSelected: boolean;
+  // TODO : 받아올 친구 정보에 따라 타입 변환
+  selectedFriends: string[];
+  selectedHeadCount: number;
+};
+
+type SelectedFriendsAction = {
+  setSelectedFriends: (state: string[]) => void;
+  getImgUrl: () => string;
+  getTitle: () => string;
+};
+
+type PersistType = (
+  config: StateCreator<SelectedFriendsState & SelectedFriendsAction>,
+  options: PersistOptions<SelectedFriendsState & SelectedFriendsAction>,
+) => StateCreator<SelectedFriendsState & SelectedFriendsAction>;
+
+export const useSelectedFriendsStore = create<
+  SelectedFriendsState & SelectedFriendsAction
+>(
+  (persist as PersistType)(
+    (set, get) => ({
       isSelected: false,
+      isSelfSelected: false,
       selectedFriends: [],
       selectedHeadCount: 0,
-      // 친구 타입
-      setSelectedFriends: (state: string[]) => {
-        set({
-          selectedFriends: [...state],
-          selectedHeadCount: state.length,
+
+      setSelectedFriends: (state) =>
+        set(() => ({
           isSelected: !(state.length === 0),
-        });
+          selectedFriends: state,
+          selectedHeadCount: state.length,
+        })),
+
+      getImgUrl: () => {
+        // 여러명일 경우
+        if (get().selectedHeadCount > 1) return peopleImgUrl;
+        if (get().selectedHeadCount === 1) return profileImgUrl;
+        // 친구의 정보 이미지 넣기
+        return defaultImgUrl;
+      },
+
+      getTitle: () => {
+        if (!get().isSelected) return '선물 받을 친구를 선택해주세요.';
+        if (get().selectedHeadCount > 1)
+          return `${get().selectedHeadCount}명의 친구에게 선물하기`;
+        if (get().isSelfSelected) return '나를 위한 선물하기';
+        return `${get().selectedFriends}에게 선물하기`;
       },
     }),
     {
