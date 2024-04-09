5ceaf7750718688178f55ef3825a69d848c03142
devkyoung2 <devkyoung2@gmail.com>
2024-04-10 00:51:44 +0900

    [feat]: 펀딩아이템 등록 모달 컴포넌트 구현(#127)

---
+++ src/layouts/Product/Buyinfo/ButtonBundles/FundingModal/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+
+import { Button } from 'components/ui/Button';
+import Modal from 'components/ui/Modal';
+
+import { formatNumberWithUnit } from 'utils/format';
+
+import { FriendsSelectorModalProps } from 'types/modal';
+
+import styles from './index.module.scss';
+
+const mockData = {
+  title: '[각인+포장] 리브르 헤어 미스트 30ml 세트(+향수 미니어처 7.5ml)',
+  option: '940 코쿤 [New & Limited]',
+  thumbImgUrl:
+    'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20220111185052_b92447cb764d470ead70b2d0fe75fe5c.jpg',
+  price: 3940000,
+};
+const FundingModal = ({
+  close,
+  isOpen,
+  scrollPos,
+}: FriendsSelectorModalProps) => {
+  const [input, setInput] = useState<string>('');
+  const handleAddFunding = close;
+
+  return (
+    isOpen && (
+      <Modal
+        scrollPos={scrollPos}
+        onClose={close}
+        isOpen={isOpen}
+        className={styles.modal}
+      >
+        <strong className={styles.modal_title}>
+          펀딩 목표 금액을 설정해주세요
+        </strong>
+        <section className={styles.wrapper_info}>
+          <div className={styles.prod_title}>{mockData.title}</div>
+          <div className={styles.prod_option}>
+            <span className={styles.ico_option}>옵션</span>
+            {mockData.option}
+          </div>
+        </section>
+        <img
+          src={mockData.thumbImgUrl}
+          alt="등록삼품썸네일"
+          className={styles.prod_thumb}
+        />
+        <section className={styles.wrapper_price}>
+          <div className={styles.txt_price}>상품 금액</div>
+          {formatNumberWithUnit(mockData.price)}
+        </section>
+        <section className={styles.wrapper_price}>
+          <div className={styles.txt_price}>
+            잔여 금액
+            <span className={styles.ico_price}>아이콘</span>
+          </div>
+          {formatNumberWithUnit(mockData.price)}
+        </section>
+        <section className={styles.wrapper_price}>
+          <div className={styles.txt_price}>펀딩 목표 금액</div>
+          <div className={styles.wrapper_input}>
+            <input
+              className={styles.input}
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              placeholder="0"
+            />
+            원
+          </div>
+        </section>
+        <Button
+          color="yellow"
+          onClick={handleAddFunding}
+          className={styles.btn_add}
+        >
+          등록하기
+        </Button>
+      </Modal>
+    )
+  );
+};
+
+export default FundingModal;
