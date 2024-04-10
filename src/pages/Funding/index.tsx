e059b780f42221bc7b510488b0d437fb846b0686
devkyoung2 <devkyoung2@gmail.com>
2024-04-10 22:06:11 +0900

    [feat]: 펀딩 페이지 레이아웃 구현(#117)

--- src/pages/Funding/index.tsx
+++ src/pages/Funding/index.tsx
@@ -1,5 +1,19 @@
+import styles from './index.module.scss';
+
+const mockdata = {
+  userName: '보경',
+};
 const Funding = () => {
-  return <>펀딩</>;
+  return (
+    <>
+      <div className={styles.title}>
+        {`${mockdata.userName}님의 \n펀딩아이템`}
+      </div>
+
+      <div>펀딩영역</div>
+      <div>펀딩 아이템 추천 영역</div>
+    </>
+  );
 };
 
 export default Funding;
