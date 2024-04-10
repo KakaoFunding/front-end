12435a11abadb52b571647b74d7addd0c81cc12c
devkyoung2 <devkyoung2@gmail.com>
2024-04-10 20:47:41 +0900

    [feat]: My 선물하기 페이지 프로필 컴포넌트 구현(#115)

--- src/layouts/GiftBox/Profile/index.tsx
+++ src/layouts/GiftBox/Profile/index.tsx
@@ -1,5 +1,25 @@
+import styles from './index.module.scss';
+
+const mockdata = {
+  imgUrl:
+    'https://gift-s.kakaocdn.net/dn/gift/images/m640/bg_profile_default.png',
+  userName: '보경',
+  month: 5,
+  day: 7,
+};
 const Profile = () => {
-  return <div>프로필</div>;
+  // <ProfileImg size='xl' />
+  return (
+    <section className={styles.wrapper_profile}>
+      <img alt="프로필" src={mockdata.imgUrl} className={styles.img} />
+      <div className={styles.txt_name}>
+        {mockdata.userName}
+        <div
+          className={styles.txt_brith}
+        >{`내 생일은 ${mockdata.month}월 ${mockdata.day}일`}</div>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
