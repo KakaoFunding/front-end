7faf69729b74a9477e51a7dda5b48e94d54b7de5
devkyoung2 <devkyoung2@gmail.com>
2024-04-13 01:58:51 +0900

    [refactor]: 렌더링 될 문자열을 관리할 객체 생성(#105)

--- src/components/feature/EmptyItem/index.tsx
+++ src/components/feature/EmptyItem/index.tsx
@@ -4,7 +4,18 @@ type EmptyItemProps = {
   type: 'wish' | 'funding';
 };
 
-const choonsikImgUrl = '/src/assets/bg_choonsik.png';
+const choonsikImgUrl = 'src/assets/bg_choonsik.png';
+
+const EMPTY_ITEM_TEXT = {
+  wish: {
+    title: `위시리스트에\n받고싶은 선물을 담아보세요~!`,
+    description: `선물 고민에 빠진 친구와, 소중한 나를 위해\n 내 취향에 맞는 선물을 위시❤️해보세요.`,
+  },
+  funding: {
+    title: `등록된 펀딩 아이템이 없어요`,
+    description: `펀딩받고 싶은 선물🎁이 있나요?\n내 취향에 맞는 선물을 등록해보세요~!`,
+  },
+};
 
 const EmptyItem = ({ type }: EmptyItemProps) => {
   return (
@@ -15,15 +26,9 @@ const EmptyItem = ({ type }: EmptyItemProps) => {
         className={styles.img_choonsik}
       />
       <strong className={styles.txt_title}>
-        {type === 'wish' && `위시리스트에\n받고싶은 선물을 담아보세요~!`}
-        {type === 'funding' && `등록된 펀딩 아이템이 없어요`}
+        {EMPTY_ITEM_TEXT[type].title}
       </strong>
-      <p className={styles.txt_desc}>
-        {type === 'wish' &&
-          `선물 고민에 빠진 친구와, 소중한 나를 위해\n 내 취향에 맞는 선물을 위시❤️해보세요.`}
-        {type === 'funding' &&
-          `펀딩받고 싶은 선물🎁이 있나요?\n내 취향에 맞는 선물을 등록해보세요~!`}
-      </p>
+      <p className={styles.txt_desc}>{EMPTY_ITEM_TEXT[type].description} </p>
     </section>
   );
 };
