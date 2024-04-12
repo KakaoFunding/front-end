eba710b8afe244eb710272b44d58eb89ee53ee26
devkyoung2 <devkyoung2@gmail.com>
2024-04-13 01:14:38 +0900

    [feat]: 펀딩하기 버튼 클릭시 결제페이지로 라우팅(#124)

--- src/layouts/Home/Receiver/FriendsFunding/index.tsx
+++ src/layouts/Home/Receiver/FriendsFunding/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 
 import Thumbnail from 'components/feature/ProductItem/Thumbnail';
 import { Button } from 'components/ui/Button';
@@ -12,6 +13,7 @@ const data = {
     'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240318095141_80a46f7dfcee4aecae72311ac9c52e7d.jpg',
   brand: '스타벅스',
   name: '스타벅스 딸기 라떼 T + 피스타치오 핑크 롤',
+  productId: 9467308,
   productPrice: 1000000,
   fundingPrice: 98000,
 };
@@ -38,9 +40,9 @@ const FriendsFunding = () => {
             <div className={styles.txt_info}>
               <p className={styles.txt_default}>
                 목표 달성까지
-                <p
+                <span
                   className={styles.txt_point}
-                >{`${WHITE_SPACE}${formatNumberWithComma(data.productPrice - data.fundingPrice)}`}</p>
+                >{`${WHITE_SPACE}${formatNumberWithComma(data.productPrice - data.fundingPrice)}`}</span>
                 원 남았어요
               </p>
               <p className={styles.txt_default}>
@@ -54,13 +56,12 @@ const FriendsFunding = () => {
             <div style={{ backgroundColor: `blue`, margin: '20px 0' }}>
               프로그레스바
             </div>
-            <Button
-              onClick={handleFunding}
-              color="yellow"
-              className={styles.btn_funding}
-            >
-              펀딩하기
-            </Button>
+            {/* TODO : 결제페이지 props에 맞게 필요한 데이터 전달 */}
+            <Link to="/bill" state={{ productId: data.productId, self: false }}>
+              <Button color="yellow" className={styles.btn_funding}>
+                펀딩하기
+              </Button>
+            </Link>
           </div>
         </section>
       </section>
