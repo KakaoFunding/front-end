866c863ba2b46c60059cd6d755acaa232e8844e1
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-14 00:05:28 +0900

    [refactor]: 불필요한 요소 제거 (#137)
    li 태그 내 li 태그가 존재하는 문제 해결
    
    기존: ul 태그 아래 브랜드 및 상품이 하나의 li 태그로 묶여 있음
    변경: ul 태그 삭제, 브랜드 및 상품을 하나의 article 태그로 묶음

--- src/layouts/SearchResult/SearchContents/BrandTab/index.tsx
+++ src/layouts/SearchResult/SearchContents/BrandTab/index.tsx
@@ -68,52 +68,50 @@ const BrandTab = ({ tabName, keyword }: BrandTabProps) => {
         {/* 정렬 */}
       </ResultTabTitle>
 
-      <ul>
-        {items.map(({ brand, products }) => (
-          <li key={brand.brandId} className={styles.item_card}>
-            <BrandCard
-              brandId={brand.brandId}
-              name={brand.name}
-              iconPhoto={brand.iconPhoto}
-              category=""
-              size="large"
-            />
-            <Slider
-              arrows={products.length > SLOTS_PER_SLIDE}
-              slidesToShow={SLOTS_PER_SLIDE}
-              slidesToScroll={SLOTS_PER_SLIDE}
-              slide="ul"
-              infinite={false}
-              prevArrow={<SliderArrowButton arrowType="prev" />}
-              nextArrow={<SliderArrowButton arrowType="next" />}
-              className={styles.wrapper_slider}
-            >
-              {
-                // 브랜드 별 상품 목록 생성
-                products.map((product) => (
-                  <li key={product.id}>
-                    <ColumnProductItem size="medium" product={product} />
-                  </li>
-                ))
-              }
-              {
-                // 한 슬라이드를 다 채우지 못하면, 그만큼 빈 슬롯 생성
-                products.length < SLOTS_PER_SLIDE &&
-                  Array.from({ length: SLOTS_PER_SLIDE - products.length }).map(
-                    (_, i) => <li key={`empty_slot_${i + 1}`} aria-hidden />,
-                  )
-              }
-              {
-                // 상품이 더 있으면, 브랜드 더보기 슬롯 생성
-                products.length === MAX_SLOTS && (
-                  <BrandMoreSlot size="medium" brandId={brand.brandId} />
+      {items.map(({ brand, products }) => (
+        <article key={brand.brandId} className={styles.area_card}>
+          <BrandCard
+            brandId={brand.brandId}
+            name={brand.name}
+            iconPhoto={brand.iconPhoto}
+            category=""
+            size="large"
+          />
+          <Slider
+            arrows={products.length > SLOTS_PER_SLIDE}
+            slidesToShow={SLOTS_PER_SLIDE}
+            slidesToScroll={SLOTS_PER_SLIDE}
+            slide="ul"
+            infinite={false}
+            prevArrow={<SliderArrowButton arrowType="prev" />}
+            nextArrow={<SliderArrowButton arrowType="next" />}
+            className={styles.wrapper_slider}
+          >
+            {
+              // 브랜드 별 상품 목록 생성
+              products.map((product) => (
+                <li key={product.id}>
+                  <ColumnProductItem size="medium" product={product} />
+                </li>
+              ))
+            }
+            {
+              // 한 슬라이드를 다 채우지 못하면, 그만큼 빈 슬롯 생성
+              products.length < SLOTS_PER_SLIDE &&
+                Array.from({ length: SLOTS_PER_SLIDE - products.length }).map(
+                  (_, i) => <li key={`empty_slot_${i + 1}`} aria-hidden />,
                 )
-              }
-            </Slider>
-          </li>
-        ))}
-        {isLoading && <Spinner />}
-      </ul>
+            }
+            {
+              // 상품이 더 있으면, 브랜드 더보기 슬롯 생성
+              products.length === MAX_SLOTS && (
+                <BrandMoreSlot size="medium" brandId={brand.brandId} />
+              )
+            }
+          </Slider>
+        </article>
+      ))}
+      {isLoading && <Spinner />}
       <div ref={observingTarget} />
     </section>
   );
