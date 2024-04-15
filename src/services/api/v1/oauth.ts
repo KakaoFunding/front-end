f64afe2908c97a822788eff96327ba42d667711a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-04-15 21:15:20 +0900

    [feat]: 로그인 엑세스 토큰 발급 기능 구현 (#147)

--- src/services/api/v1/oauth.ts
+++ src/services/api/v1/oauth.ts
@@ -1,12 +1,16 @@
-import {
+import axios, {
   AxiosRequestHeaders,
   AxiosResponse,
-  RawAxiosResponseHeaders,
+  // RawAxiosResponseHeaders,
 } from 'axios';
+
+import { useAuthStore } from 'store/useAuthStore';
 
 import { User } from 'types/user';
 
-import { apiV1 } from '.';
+import { Data } from './service';
+
+// import { apiV1 } from '.';
 
 type LoginRequestProps = {
   code: string;
@@ -17,29 +21,64 @@ type LoginResponseProps = {
   member: NonNullable<User>;
 };
 
-export const login = async ({
-  code,
-}: LoginRequestProps): Promise<AxiosResponse<LoginResponseProps>> => {
-  return new Promise((res) => {
-    const fakeApiResponse: AxiosResponse<LoginResponseProps> = {
-      data: {
-        accessToken: 'fakeAccessToken',
-        member: {
-          name: 'fake guy',
-          profileUrl: 'fakeUrl',
-        },
-      },
-      status: 200,
-      statusText: 'OK',
-      headers: {} as RawAxiosResponseHeaders,
-      config: { headers: {} as AxiosRequestHeaders },
-    };
+// export const login = async ({
+//   code,
+// }: LoginRequestProps): Promise<AxiosResponse<LoginResponseProps>> => {
+//   return new Promise((res) => {
+//     const fakeApiResponse: AxiosResponse<LoginResponseProps> = {
+//       data: {
+//         accessToken: 'fakeAccessToken',
+//         member: {
+//           name: 'fake guy',
+//           profileUrl: 'fakeUrl',
+//         },
+//       },
+//       status: 200,
+//       statusText: 'OK',
+//       headers: {} as RawAxiosResponseHeaders,
+//       config: { headers: {} as AxiosRequestHeaders },
+//     };
 
-    setTimeout(() => res(fakeApiResponse), 200);
-  });
-  // apiV1.post('/oauth/login', null, {
-  //   params: { provider: 'kakao', code },
-  // });
+//     setTimeout(() => res(fakeApiResponse), 200);
+//   });
+//   // apiV1.post('/oauth/login', null, {
+//   //   params: { provider: 'kakao', code },
+//   // });
+// };
+
+// 로그인: 토큰 발급
+export const getKakaoOauthToken = async ({ code }: LoginRequestProps) => {
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+  const makeFormData = (params: { [key: string]: string }) => {
+    const searchParams = new URLSearchParams();
+    Object.keys(params).forEach((key) => {
+      searchParams.append(key, params[key]);
+    });
+
+    return searchParams;
+  };
+
+  try {
+    const res = await axios({
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      url: 'https://kauth.kakao.com/oauth/token',
+      data: makeFormData({
+        grant_type: 'authorization_code',
+        client_id: import.meta.env.VITE_REST_API_KEY,
+        // redirect_uri는 배포가 완료되면 수정되어야함
+        redirect_uri: import.meta.env.VITE_REDIRECT_URL,
+        code,
+      }),
+    });
+
+    Data.set('kakaoLogin', res.data.access_token);
+    setAccessToken(res.data.access_token);
+  } catch (error) {
+    console.warn(error);
+  }
 };
 
 export const logout = async (): Promise<AxiosResponse<LoginResponseProps>> => {
