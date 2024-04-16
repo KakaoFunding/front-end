53f3eb01009b598054510149a85d5e3fdffc630d
devkyoung2 <devkyoung2@gmail.com>
2024-04-17 02:29:46 +0900

    [feat]: 더보기 버튼 컴포넌트 구현(#120)

--- src/layouts/MyPage/Funding/FundingRanking/index.tsx
+++ src/layouts/MyPage/Funding/FundingRanking/index.tsx
@@ -1,9 +1,15 @@
+import MoreButton from 'components/ui/Button/MoreButton';
+
 import styles from './index.module.scss';
 
 const FundingRanking = () => {
+  const handleClick = () => {
+    // 더보기로직
+  };
   return (
     <section className={styles.area_ranking}>
       <strong className={styles.txt_title}>펀딩 랭킹</strong>
+      <MoreButton onClick={handleClick} size="small" />
     </section>
   );
 };
