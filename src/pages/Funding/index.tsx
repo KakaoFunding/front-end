564baffea766eaabbb4ca7ecdd6c63432124d1d9
devkyoung2 <devkyoung2@gmail.com>
2024-04-16 15:19:38 +0900

    [feat]: 펀딩 여부에 따른 default 컴포넌트 추가(#118)

--- src/pages/Funding/index.tsx
+++ src/pages/Funding/index.tsx
@@ -1,10 +1,11 @@
+import EmptyItem from 'components/feature/EmptyItem';
 import FundingItem from 'layouts/MyPage/Funding/FundingItem';
 
 import styles from './index.module.scss';
 
 // 목데이터
 const data = {
-  hasFunding: true,
+  hasFunding: false,
   userName: '보경',
   imgUrl:
     'https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20240412000831_223be6cfd2eb40e3bbaf238fca8a56e9',
@@ -21,7 +22,6 @@ const Funding = () => {
   return (
     <>
       <div className={styles.title}>{`${data.userName}님의 \n펀딩아이템`}</div>
-      {/* TODO : default 컴포넌트 추가 */}
       {data.hasFunding ? (
         <FundingItem
           imgUrl={data.imgUrl}
@@ -32,7 +32,9 @@ const Funding = () => {
           brandThumbnail={data.fundingItemBrandThumbnail}
           brandId={data.brandId}
         />
-      ) : null}
+      ) : (
+        <EmptyItem type="funding" />
+      )}
       <div>펀딩 아이템 추천 영역</div>
     </>
   );
