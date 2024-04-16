c0523f5b7cbd67b6797f76ff0c29c294988ebfe8
ohgus <123801385+ohgus@users.noreply.github.com>
2024-04-16 17:06:07 +0900

    [refactor]: 엑세스 토큰 발급 로직 수정, 로그인 로직 수정
    엑세스 토큰 발급 로직 변경 사항
    - content-type value 수정
    - data 객체 하드 코딩으로 변경
    - client-secret 추가
    - 발급받은 엑세스 토큰 반환
    
    로그인 로직 변경 사항
    - 요청에 인가 코드 대신 소셜 엑세스 토큰을 포함
    (#147)

--- src/services/api/v1/oauth.ts
+++ src/services/api/v1/oauth.ts
@@ -4,16 +4,18 @@ import axios, {
   // RawAxiosResponseHeaders,
 } from 'axios';
 
-import { useAuthStore } from 'store/useAuthStore';
-
 import { User } from 'types/user';
 
 import { Data } from './service';
 
-// import { apiV1 } from '.';
+import { apiV1 } from '.';
+
+type TokenRequestProps = {
+  code: string;
+};
 
 type LoginRequestProps = {
-  code: string;
+  socialAccessToken: string;
 };
 
 type LoginResponseProps = {
@@ -21,64 +23,44 @@ type LoginResponseProps = {
   member: NonNullable<User>;
 };
 
-// export const login = async ({
-//   code,
-// }: LoginRequestProps): Promise<AxiosResponse<LoginResponseProps>> => {
-//   return new Promise((res) => {
-//     const fakeApiResponse: AxiosResponse<LoginResponseProps> = {
-//       data: {
-//         accessToken: 'fakeAccessToken',
-//         member: {
-//           name: 'fake guy',
-//           profileUrl: 'fakeUrl',
-//         },
-//       },
-//       status: 200,
-//       statusText: 'OK',
-//       headers: {} as RawAxiosResponseHeaders,
-//       config: { headers: {} as AxiosRequestHeaders },
-//     };
-
-//     setTimeout(() => res(fakeApiResponse), 200);
-//   });
-//   // apiV1.post('/oauth/login', null, {
-//   //   params: { provider: 'kakao', code },
-//   // });
-// };
-
 // 로그인: 토큰 발급
-export const getKakaoOauthToken = async ({ code }: LoginRequestProps) => {
-  const setAccessToken = useAuthStore((state) => state.setAccessToken);
-  const makeFormData = (params: { [key: string]: string }) => {
-    const searchParams = new URLSearchParams();
-    Object.keys(params).forEach((key) => {
-      searchParams.append(key, params[key]);
-    });
-
-    return searchParams;
-  };
+// eslint-disable-next-line consistent-return
+export const getKakaoOauthToken = async ({ code }: TokenRequestProps) => {
+  const API_KEY = import.meta.env.VITE_REST_API_KEY;
+  const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URL;
+  const CLIENT_SECRET_KEY = import.meta.env.VITE_CLIENT_SECRET_KEY;
 
   try {
     const res = await axios({
       method: 'POST',
       headers: {
-        'content-type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
       },
       url: 'https://kauth.kakao.com/oauth/token',
-      data: makeFormData({
+      data: {
         grant_type: 'authorization_code',
-        client_id: import.meta.env.VITE_REST_API_KEY,
-        // redirect_uri는 배포가 완료되면 수정되어야함
-        redirect_uri: import.meta.env.VITE_REDIRECT_URL,
+        client_id: API_KEY,
+        redirect_uri: REDIRECT_URI,
         code,
-      }),
+        client_secret: CLIENT_SECRET_KEY,
+      },
     });
 
-    Data.set('kakaoLogin', res.data.access_token);
-    setAccessToken(res.data.access_token);
+    Data.set('socialToken', res.data.access_token);
+    const token = res.data.access_token;
+    return token;
   } catch (error) {
     console.warn(error);
   }
+};
+
+// 서버에 소셜토큰을 담아서 로그인 요청
+export const login = async ({
+  socialAccessToken,
+}: LoginRequestProps): Promise<AxiosResponse<LoginResponseProps>> => {
+  return apiV1.post('/oauth/login', null, {
+    params: { provider: 'kakao', socialAccessToken },
+  });
 };
 
 export const logout = async (): Promise<AxiosResponse<LoginResponseProps>> => {
