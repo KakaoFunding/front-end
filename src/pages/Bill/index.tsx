aa7166937dca2b2dfd49eb81cd457db90c041c28
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-19 01:03:20 +0900

    [feat]: 예상 결제 금액 API 호출 (#110)

--- src/pages/Bill/index.tsx
+++ src/pages/Bill/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
 import MainWrapper from 'components/ui/MainWrapper';
@@ -6,10 +7,34 @@ import GiftDetail from 'layouts/Bill/GiftDetail';
 import MessageCard from 'layouts/Bill/MessageCard';
 import PaymentDetail from 'layouts/Bill/PaymentDetail';
 
+import { useAxios } from 'hooks/useAxios';
+
+import { ResponseExpectedPaymentAmount } from 'types/payment';
+
 import styles from './index.module.scss';
 
 const Bill = () => {
   const { type } = useParams();
+
+  const { data, sendRequest } = useAxios<ResponseExpectedPaymentAmount>({
+    method: 'post',
+    url: '/payments/preview',
+    data: [
+      // 임시 데이터
+      {
+        productId: 386515,
+        quantity: 1,
+      },
+      {
+        productId: 2274,
+        quantity: 3,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
 
   if (type !== 'gift' && type !== 'funding') {
     return <Navigate to="/NotFound" />;
