a01403748d0ea560c9fecb35b61b7144b87ed451
devkyoung2 <devkyoung2@gmail.com>
2024-04-19 20:50:56 +0900

    [feat]: 랭킹 뱃지 적용(#120)
    - 랭킹뱃지 적용
    - 목데이터 값 변경
    - 여백 값 변경

--- src/layouts/MyPage/Funding/FundingRanking/Ranker/index.tsx
+++ src/layouts/MyPage/Funding/FundingRanking/Ranker/index.tsx
@@ -1,20 +1,23 @@
 import ProfileImg from 'components/feature/ProfileImg';
+import RankBadge from 'components/feature/RankBadge';
 
 import styles from './index.module.scss';
 
 type RankerProps = {
+  rank: number;
   key: number;
   name: string;
   percent: string;
 };
 
-const Ranker = ({ name, percent, key }: RankerProps) => {
+const Ranker = ({ rank, name, percent, key }: RankerProps) => {
   return (
     <li className={styles.item_ranker} key={key}>
       <ProfileImg size="l" />
+      <RankBadge rank={rank} />
       <p className={styles.txt_default}>{name}</p>
       <p className={styles.txt_default}>
-        <span className={styles.txt_point}>{percent}</span>기여
+        <span className={styles.txt_point}>{percent}</span> 기여
       </p>
     </li>
   );
--- src/layouts/MyPage/Funding/FundingRanking/index.tsx
+++ src/layouts/MyPage/Funding/FundingRanking/index.tsx
@@ -9,56 +9,13 @@ import styles from './index.module.scss';
 const data = {
   productPrice: 987000,
   ranker: [
-    {
-      id: 1,
-      name: '철수',
-      amount: 200000,
-    },
-    {
-      id: 2,
-      name: '짱구',
-      amount: 100000,
-    },
-    {
-      id: 3,
-      name: '유리',
-      amount: 50000,
-    },
-    {
-      id: 4,
-      name: '맹구',
-      amount: 10000,
-    },
-    {
-      id: 5,
-      name: '훈이',
-      amount: 5000,
-    },
-    {
-      id: 1,
-      name: '철수',
-      amount: 200000,
-    },
-    {
-      id: 2,
-      name: '짱구',
-      amount: 100000,
-    },
-    {
-      id: 3,
-      name: '유리',
-      amount: 50000,
-    },
-    {
-      id: 4,
-      name: '맹구',
-      amount: 10000,
-    },
-    {
-      id: 5,
-      name: '훈이',
-      amount: 5000,
-    },
+    { rank: 1, id: 1, name: '철수', amount: 200000 },
+    { rank: 2, id: 2, name: '신짱구', amount: 100000 },
+    { rank: 3, id: 3, name: '유리', amount: 50000 },
+    { rank: 4, id: 4, name: '맹구', amount: 10000 },
+    { rank: 5, id: 5, name: '훈이', amount: 5000 },
+    { rank: 6, id: 6, name: '신짱아', amount: 1000 },
+    { rank: 7, id: 7, name: '흰둥이', amount: 500 },
   ],
 };
 
@@ -72,6 +29,7 @@ const FundingRanking = () => {
       <ul className={styles.list_ranker}>
         {data.ranker.map((ranker) => (
           <Ranker
+            rank={ranker.rank}
             name={ranker.name}
             percent={formatNumberToPercent(ranker.amount, data.productPrice)}
             key={ranker.id}
