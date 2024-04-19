aef8dfed48e9f6e943b17222288d2c2386247f2f
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-19 13:14:18 +0900

    [refactor]: 기타결제 삭제 (#110)

--- src/components/feature/PaymentDetail/index.tsx
+++ src/components/feature/PaymentDetail/index.tsx
@@ -11,10 +11,6 @@ const PaymentDetail = ({ totalPrice }: PaymentDetailProps) => {
   const PAY_ID = {
     KAKAO_MONEY: 'kakao_pay_money',
     KAKAO_CARD: 'kakao_pay_card',
-    SAMSUNG_PAY: 'samsung_pay',
-    CARD: 'card',
-    MOBILE: 'mobile',
-    VACNT: 'virtual_account',
   };
 
   return (
@@ -32,26 +28,6 @@ const PaymentDetail = ({ totalPrice }: PaymentDetailProps) => {
           <label htmlFor={PAY_ID.KAKAO_CARD}>
             <input type="radio" id={PAY_ID.KAKAO_CARD} name={PAY_METHOD} />
             카카오페이카드
-          </label>
-        </div>
-
-        <div className={styles.wrapper_box}>
-          <h4 className={styles.txt_subtitle}>기타결제</h4>
-          <label htmlFor={PAY_ID.SAMSUNG_PAY}>
-            <input type="radio" id={PAY_ID.SAMSUNG_PAY} name={PAY_METHOD} />
-            삼성페이
-          </label>
-          <label htmlFor={PAY_ID.CARD}>
-            <input type="radio" id={PAY_ID.CARD} name={PAY_METHOD} />
-            신용/체크카드
-          </label>
-          <label htmlFor={PAY_ID.MOBILE}>
-            <input type="radio" id={PAY_ID.MOBILE} name={PAY_METHOD} />
-            휴대폰
-          </label>
-          <label htmlFor={PAY_ID.VACNT}>
-            <input type="radio" id={PAY_ID.VACNT} name={PAY_METHOD} />
-            무통장 입금
           </label>
         </div>
 
