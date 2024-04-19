d41ca33b60bead70688701009ad5dc27decca353
ohgus <123801385+ohgus@users.noreply.github.com>
2024-04-20 01:45:23 +0900

    [fix]: 현재 사용하지 않는 값 주석처리 - 테스트 이후에 수정되어야 하지만 빌드에서 에러가 발생하는 값들 주석처리 (#183)

--- src/services/api/v1/members.ts
+++ src/services/api/v1/members.ts
@@ -1,4 +1,8 @@
-import { AxiosError, AxiosRequestHeaders, AxiosResponse } from 'axios';
+import {
+  // AxiosError,
+  AxiosRequestHeaders,
+  AxiosResponse,
+} from 'axios';
 
 // import { apiV1 } from '.';
 
@@ -9,34 +13,39 @@ type RefreshAccessTokenProps = {
 export const refreshAccessToken = async (): Promise<
   AxiosResponse<RefreshAccessTokenProps>
 > => {
-  return new Promise((res, rej) => {
-    const fakeApiResponse: AxiosResponse<RefreshAccessTokenProps> = {
-      data: {
-        accessToken: 'fakeToken',
-      },
-      status: 200,
-      statusText: 'OK',
-      headers: {} as Record<string, string>,
-      config: { headers: {} as AxiosRequestHeaders },
-    };
+  return new Promise(
+    (
+      res,
+      // rej
+    ) => {
+      const fakeApiResponse: AxiosResponse<RefreshAccessTokenProps> = {
+        data: {
+          accessToken: 'fakeToken',
+        },
+        status: 200,
+        statusText: 'OK',
+        headers: {} as Record<string, string>,
+        config: { headers: {} as AxiosRequestHeaders },
+      };
 
-    // 토큰 만료 api
-    // const fakeApiError: AxiosError<RefreshAccessTokenProps> = {
-    //   name: 'AxiosError',
-    //   message: 'Unauthorized',
-    //   response: {
-    //     data: { accessToken: 'wrongToken' }, // 에러 응답의 형태에 맞게 데이터를 설정
-    //     status: 401,
-    //     statusText: 'Unauthorized',
-    //     headers: {},
-    //     config: { headers: {} as AxiosRequestHeaders },
-    //   },
-    //   isAxiosError: true,
-    //   toJSON: () => ({ message: 'Unauthorized', name: 'AxiosError' }),
-    // };
+      // 토큰 만료 api
+      // const fakeApiError: AxiosError<RefreshAccessTokenProps> = {
+      //   name: 'AxiosError',
+      //   message: 'Unauthorized',
+      //   response: {
+      //     data: { accessToken: 'wrongToken' }, // 에러 응답의 형태에 맞게 데이터를 설정
+      //     status: 401,
+      //     statusText: 'Unauthorized',
+      //     headers: {},
+      //     config: { headers: {} as AxiosRequestHeaders },
+      //   },
+      //   isAxiosError: true,
+      //   toJSON: () => ({ message: 'Unauthorized', name: 'AxiosError' }),
+      // };
 
-    setTimeout(() => res(fakeApiResponse), 200);
-  });
+      setTimeout(() => res(fakeApiResponse), 200);
+    },
+  );
 
   // const response = await apiV1.post('/members/token');
   // return response;
