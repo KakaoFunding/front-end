f84f6fb1c13c89e03c149945c219f9d8132793ec
devkyoung2 <devkyoung2@gmail.com>
2024-04-19 17:43:01 +0900

    [feat]: 상품 타입 지정(#158)

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,3 +2,20 @@ export type Option = {
   id: number;
   name: string;
 };
+
+export type Product = {
+  productId: number;
+  name: string;
+  price: number;
+  type: string;
+  productName: string;
+  options: Option[];
+  brandName: string;
+  origin: string;
+  manufacturer: string;
+  tel: string;
+  deliverDescription: string;
+  billingNotice: string;
+  caution: string;
+  productThumbnails: string[];
+};
