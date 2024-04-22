986b042c634b9de2082f92264e4d72002f2c870f
devkyoung2 <devkyoung2@gmail.com>
2024-04-23 00:43:16 +0900

    [feat]: FundingDetail 컴포넌트 구현(#203)

--- src/layouts/FundingPayment/FundingDetail/index.tsx
+++ src/layouts/FundingPayment/FundingDetail/index.tsx
@@ -1,5 +1,51 @@
+import { useState } from 'react';
+
+import PaymentItem from 'components/feature/PaymentItem';
+
+import { formatNumberWithComma } from 'utils/format';
+
+import styles from './index.module.scss';
+
+const data = {
+  fundingGoalPrice: 519000,
+};
+
 const FundingDetail = () => {
-  return <div>FundingDetail</div>;
+  const [input, setInput] = useState();
+
+  const handleChange = () => {};
+  return (
+    <section className={styles.area_detail}>
+      <strong className={styles.txt_title}>펀딩내역</strong>
+      <div className={styles.wrapper_funding_item}>
+        <PaymentItem />
+        <p className={styles.wrapper_desc}>
+          <span className={styles.txt_price}>펀딩 목표 금액</span>
+          <span className={styles.num_price}>
+            {formatNumberWithComma(data.fundingGoalPrice)}
+          </span>
+          원
+        </p>
+        <p className={styles.wrapper_desc}>
+          <span className={styles.txt_price}>잔여 금액</span>
+          <span className={styles.num_price}>
+            {formatNumberWithComma(data.fundingGoalPrice)}
+          </span>
+          원
+        </p>
+        <p className={styles.wrapper_desc}>
+          <span className={styles.txt_price}>기여할 금액</span>
+          <input
+            className={styles.input}
+            onChange={handleChange}
+            value={input}
+            placeholder="0"
+          />
+          원
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default FundingDetail;
