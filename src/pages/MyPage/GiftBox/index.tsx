08d323f8104c3f79a9fa0cf809bcb1c084ad4bde
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-23 23:56:05 +0900

    [feat]: 선물함 레이아웃 구현 (#189)

--- src/pages/MyPage/GiftBox/index.tsx
+++ src/pages/MyPage/GiftBox/index.tsx
@@ -1,5 +1,28 @@
+import Tabs from 'components/ui/Tabs';
+import UnavailableGiftTab from 'layouts/MyPage/GiftBox/UnavailableGiftTab';
+
+import { Tab } from 'types/tab';
+
 const GiftBox = () => {
-  return <>선물함</>;
+  const tabs: Tab[] = [
+    {
+      id: 0,
+      name: '사용가능',
+      content: <p>사용가능</p>,
+    },
+    {
+      id: 1,
+      name: '사용완료',
+      content: <UnavailableGiftTab />,
+    },
+  ];
+
+  return (
+    <>
+      <h1>사용가능한 선물이 n개 남아있어요.</h1>
+      <Tabs initialTabId={0} mode="product_list" tabs={tabs} />
+    </>
+  );
 };
 
 export default GiftBox;
