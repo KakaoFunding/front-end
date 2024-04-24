d324f596bd405deac7c49be2c5542a153f341886
ohgus <123801385+ohgus@users.noreply.github.com>
2024-04-24 18:23:39 +0900

    [refactor]: 불필요한 식별자 기호 제거 (#208)

--- src/services/api/v1/service.ts
+++ src/services/api/v1/service.ts
@@ -1,22 +1,21 @@
-/* eslint-disable no-nested-ternary */
 // session storage
 export const Data = {
-  set: ($name: string, $value: string) => {
-    window.sessionStorage.setItem($name, $value);
+  set: (name: string, value: string) => {
+    window.sessionStorage.setItem(name, value);
   },
 
-  get: ($name: string) => {
+  get: (name: string) => {
     try {
-      const data = window.sessionStorage.getItem($name);
+      const data = window.sessionStorage.getItem(name);
 
       return data !== 'undefined' && data !== null ? JSON.parse(data) : data;
     } catch (error) {
-      return window.sessionStorage.getItem($name);
+      return window.sessionStorage.getItem(name);
     }
   },
 
-  remove: ($name: string) => {
-    window.sessionStorage.removeItem($name);
+  remove: (name: string) => {
+    window.sessionStorage.removeItem(name);
   },
 
   clear: () => {
