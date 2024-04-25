048c0436bf470dd1c52672bff72bf4cfead9c42b
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-25 21:30:48 +0900

    [feat]: 선물함 페이지 헤더 텍스트 구현 (#188)

--- src/pages/MyPage/GiftBox/index.tsx
+++ src/pages/MyPage/GiftBox/index.tsx
@@ -4,6 +4,8 @@ import UnavailableGiftTab from 'layouts/MyPage/GiftBox/UnavailableGiftTab';
 
 import { Tab } from 'types/tab';
 
+import styles from './index.module.scss';
+
 const GiftBox = () => {
   const tabs: Tab[] = [
     {
@@ -20,7 +22,11 @@ const GiftBox = () => {
 
   return (
     <>
-      <h1>사용가능한 선물이 n개 남아있어요.</h1>
+      <h1 className={styles.txt_title}>
+        사용가능한 선물이
+        <br />
+        n개 남아있어요.
+      </h1>
       <Tabs initialTabId={0} mode="product_list" tabs={tabs} />
     </>
   );
