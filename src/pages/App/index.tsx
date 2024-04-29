29ae453de5085d8ea609179ea53ee74e5a1083ce
ohgus <123801385+ohgus@users.noreply.github.com>
2024-04-29 21:09:51 +0900

    [feat]: 피커 메서드 사용을 위해 App 컴포넌트에 sdk 연결 (#217)

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -5,6 +5,9 @@ import Header from 'layouts/App/Header';
 
 import styles from './index.module.scss';
 
+window.Kakao.init(import.meta.env.VITE_JAVASCRIPT_KEY);
+console.log(window.Kakao.isInitialized());
+
 const App = () => (
   <>
     <Header />
