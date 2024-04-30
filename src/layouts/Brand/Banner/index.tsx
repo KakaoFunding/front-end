43389374c32e0fad94f03f645fd0407d1262d053
uraflower <82873315+uraflower@users.noreply.github.com>
2024-04-30 18:13:18 +0900

    [feat]: 브랜드 정보 조회 API 연동 (#199)

--- src/layouts/Brand/Banner/index.tsx
+++ src/layouts/Brand/Banner/index.tsx
@@ -1,26 +1,42 @@
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { useAxios } from 'hooks/useAxios';
+
+import { Brand } from 'types/Brand';
+
 import styles from './index.module.scss';
 
 type BannerProps = {
   brandId: number;
 };
 
-const brand = {
-  name: '브랜드명',
-  photo:
-    'https://img1.kakaocdn.net/thumb/C50x50@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fgift_brand%2F20230206190514_443b0fd3f81445da9feffd65febc8b55.jpg',
-};
+const Banner = ({ brandId }: BannerProps) => {
+  const {
+    data: brand,
+    error,
+    sendRequest,
+  } = useAxios<Brand>({
+    method: 'get',
+    url: `/brands/${brandId}`,
+  });
 
-const Banner = (brandId: BannerProps) => {
-  // 브랜드 로고, 브랜드명 fetch
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
+  if (error) {
+    Navigate({ to: 'NotFound' });
+  }
 
   return (
     <section className={styles.area_banner}>
       <div className={styles.wrapper_content}>
         <span className={styles.wrapper_logo}>
-          <img src={brand.photo} alt={brand.name} />
+          <img src={brand?.iconPhoto} alt={brand?.name} />
         </span>
         <span>
-          <strong className={styles.txt_name}>{brand.name}</strong>
+          <strong className={styles.txt_name}>{brand?.name}</strong>
         </span>
       </div>
     </section>
