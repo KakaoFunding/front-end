65725de10872cef1c3a61089efd953341c1e24ac
devkyoung2 <devkyoung2@gmail.com>
2024-05-01 22:54:19 +0900

    [fix]: 위시 아이템의 클릭 범위가 겹치는 문제 해결(#197)
    - padding값을 margin으로 변경하여 위시 아이템 요소의 클릭 범위가 겹치지 않도록 함

--- src/layouts/Home/Receiver/FriendWish/FriendWishItem/index.tsx
+++ src/layouts/Home/Receiver/FriendWish/FriendWishItem/index.tsx
@@ -12,27 +12,29 @@ import styles from './index.module.scss';
 
 const FriendWishItem = ({ product }: { product: ProductItem }) => {
   return (
-    <Link to={`/product/${product.productId}`}>
-      <div className={styles.item_prod}>
-        <Thumbnail alt={product.name} src={product.photo} size="small" />
-        <div className={styles.info_prod}>
-          <p className={styles.txt_brand}>{product.brandName}</p>
-          <strong className={styles.txt_title}>{product.name}</strong>
-          <p className={styles.txt_price}>
-            {formatNumberWithComma(product.price)}
-            <span className={styles.txt_unit}>원</span>
-          </p>
-          <div className={styles.wrapper_btn}>
-            <CartButton id={product.productId} />
-            <WishButton
-              id={product.productId}
-              isWished={product.wished}
-              wishCount={product.wishCount}
-            />
+    <div className={styles.wrapper_wish_item}>
+      <Link to={`/product/${product.productId}`}>
+        <div className={styles.item_prod}>
+          <Thumbnail alt={product.name} src={product.photo} size="small" />
+          <div className={styles.info_prod}>
+            <p className={styles.txt_brand}>{product.brandName}</p>
+            <strong className={styles.txt_title}>{product.name}</strong>
+            <p className={styles.txt_price}>
+              {formatNumberWithComma(product.price)}
+              <span className={styles.txt_unit}>원</span>
+            </p>
+            <div className={styles.wrapper_btn}>
+              <CartButton id={product.productId} />
+              <WishButton
+                id={product.productId}
+                isWished={product.wished}
+                wishCount={product.wishCount}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
