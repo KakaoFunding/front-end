91640b9ef940ff0f6b058c7639bf5626192ed8a9
devkyoung2 <devkyoung2@gmail.com>
2024-05-03 20:54:05 +0900

    [refactor]: 상품 타입 분리(#158)
    상품의 타입을 상품 탭에 따라 다음 두가지 항목으로 분리
    - ProductDescriptionResponse
    - ProductDetailResponse

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,14 +3,31 @@ export type Option = {
   name: string;
 };
 
-export type ProductResponse = {
+export type ProductDescriptionResponse = {
+  productId: number;
+  name: string;
+  price: number;
+  type: string;
+  description: string;
+  descriptionPhotos: string[];
+  productName: string;
+  options: string[];
+  productThumbnails: string[];
+  brandName: string;
+  brandId: number;
+  brandThumbnail: string;
+};
+
+export type ProductDetailResponse = {
   productId: number;
   name: string;
   price: number;
   type: string;
   productName: string;
-  options: Option[];
+  options: string[];
   brandName: string;
+  brandId: number;
+  brandThumbnail: string;
   origin: string;
   manufacturer: string;
   tel: string;
