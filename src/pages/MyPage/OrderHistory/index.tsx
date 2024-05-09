1276f2f4a3433891ad23348e146d1bf906c4d21f
devkyoung2 <devkyoung2@gmail.com>
2024-05-09 18:17:42 +0900

    [feat]:주문내역 페이지 레이아웃 구현 (#229)

--- src/pages/MyPage/OrderHistory/index.tsx
+++ src/pages/MyPage/OrderHistory/index.tsx
@@ -1,5 +1,20 @@
+import styles from './index.module.scss';
+
+const data = {
+  userName: '보경',
+};
+
 const OrderHistory = () => {
-  return <>주문내역</>;
+  return (
+    <>
+      <div className={styles.title}>{`${data.userName}님의 \n주문내역`}</div>
+      <ul>
+        <li>주문내역1</li>
+        <li>주문내역2</li>
+        <li>주문내역3</li>
+      </ul>
+    </>
+  );
 };
 
 export default OrderHistory;
