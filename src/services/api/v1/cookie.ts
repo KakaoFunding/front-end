71171c5b67c847a0258a59aa25746bf4d2073e7a
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-14 01:30:50 +0900

    [refactor]: react-cookie 적용 (#241)

--- src/services/api/v1/cookie.ts
+++ src/services/api/v1/cookie.ts
@@ -1,9 +1,11 @@
-export const getCookie = (key: string) => {
-  console.log(document.cookie);
-  const cookies = document.cookie.split(';').map((el) => el.split('='));
-  console.log(cookies);
-  const foundCookie = cookies.find((cookie) => cookie[0] === key);
-  console.log(foundCookie);
+// 린트 논의 필요
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Cookies } from 'react-cookie';
 
-  return foundCookie ? JSON.parse(foundCookie[1]) : null;
+const cookies = new Cookies();
+
+export const getCookie = (key: string) => {
+  const foundCookie = cookies.get(key);
+  console.log(foundCookie);
+  return foundCookie;
 };
