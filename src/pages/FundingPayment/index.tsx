fee3ad2cd5a3e600b86debce13924d02b104f59c
uraflower <82873315+uraflower@users.noreply.github.com>
2024-05-13 15:46:17 +0900

    [feat]: 펀딩 결제 준비 및 성공 API 연동 (#238)

--- src/pages/FundingPayment/index.tsx
+++ src/pages/FundingPayment/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import MessageCard from 'components/feature/MessageCard';
@@ -6,8 +6,11 @@ import PaymentDetail from 'components/feature/PaymentDetail';
 import MainWrapper from 'components/ui/MainWrapper';
 import FundingDetail from 'layouts/FundingPayment/FundingDetail';
 
-import styles from './index.module.scss';
+import { useAxios } from 'hooks/useAxios';
 
+import { ResponseFundingReady, ResponseFundingSuccess } from 'types/payment';
+
+import styles from './index.module.scss';
 
 const FundingPayment = () => {
   const { state } = useLocation();
@@ -15,9 +18,47 @@ const FundingPayment = () => {
 
   const [fundingAmount, setFundingAmount] = useState<number>(0);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const { data: readyData, sendRequest: sendReady } =
+    useAxios<ResponseFundingReady>({
+      method: 'post',
+      url: '/funding/payments/ready',
+      data: {
+        fundingId,
+        amount: fundingAmount,
+      },
+    });
+
+  const { data: approveData, sendRequest: sendApprove } =
+    useAxios<ResponseFundingSuccess>({
+      method: 'post',
+      url: '/funding/payments/success',
+      data: {
+        tid: readyData?.tid,
+        pgToken: '47aa61cf839cac18f669',
+        orderNumber: readyData?.orderNumber,
+      },
+    });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    await sendReady();
   };
+
+  // handle ready
+  useEffect(() => {
+    if (!readyData) return;
+
+    console.log(readyData);
+    sendApprove();
+  }, [readyData]);
+
+  // handle success
+  useEffect(() => {
+    if (!approveData) return;
+
+    console.log(approveData);
+    // approveData 들고 결제완료 페이지로 이동
+  }, [approveData]);
 
   return (
     <MainWrapper>
--- src/types/payment.ts
+++ src/types/payment.ts
@@ -1,3 +1,5 @@
+import { ProductItem } from './productItem';
+
 export type RequestExpectedPaymentAmount = {
   productId: number;
   quantity: number;
@@ -20,4 +22,24 @@ export type ResponseFundingPreview = {
     goalAmount: number;
     remainAmount: number;
   };
+};
+
+export type RequestFundingReady = {
+  fundingId: number;
+  amount: number;
+};
+
+export type ResponseFundingReady = {
+  tid: string;
+  redirectUrl: string;
+  orderNumber: string;
+};
+
+export type ResponseFundingSuccess = {
+  receiver: {
+    name: string;
+    photoUrl: string;
+  };
+  product: Pick<ProductItem, 'brandName' | 'name' | 'photo'>;
+  attributeAmount: number;
 };
