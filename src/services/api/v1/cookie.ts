d7212134dd4293f1906d1e89d01f2197acbc3992
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-14 01:10:40 +0900

    [refactor]: 디버깅용 console.log 추가 (#241)

--- src/services/api/v1/cookie.ts
+++ src/services/api/v1/cookie.ts
@@ -1,6 +1,7 @@
 export const getCookie = (key: string) => {
   const cookies = document.cookie.split('; ').map((el) => el.split('='));
   const foundCookie = cookies.find((cookie) => cookie[0] === key);
+  console.log(foundCookie);
 
   return foundCookie ? JSON.parse(foundCookie[1]) : null;
 };
