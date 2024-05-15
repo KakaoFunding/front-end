56733921480cfeba913b54a93ec55db6f01b63ab
uraflower <82873315+uraflower@users.noreply.github.com>
2024-05-15 18:46:51 +0900

    [feat]: 결제 취소 페이지 구현 (#249)

--- src/pages/PaymentCancel/index.tsx
+++ src/pages/PaymentCancel/index.tsx
@@ -1,5 +1,30 @@
+import { Button } from 'components/ui/Button';
+
+import ErrorImage from 'assets/img_advice.png';
+
+import styles from './index.module.scss';
+
 const PaymentCancel = () => {
-  return <div>PaymentCancel</div>;
+  return (
+    <article className={styles.wrapper_page}>
+      <img src={ErrorImage} alt="error" className={styles.img_error} />
+      <div className={styles.wrapper_txt}>
+        <strong className={styles.txt_title}>결제가 중단되었습니다.</strong>
+        <p className={styles.txt_desc}>
+          주문을 다시 하시려면 &apos;다시주문&apos;을 눌러주세요.
+        </p>
+      </div>
+
+      <div>
+        <Button color="yellow" className={styles.btn}>
+          다시 주문하기
+        </Button>
+        <Button color="yellow" className={styles.btn}>
+          홈으로
+        </Button>
+      </div>
+    </article>
+  );
 };
 
 export default PaymentCancel;
