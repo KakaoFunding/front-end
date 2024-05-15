446723df3256aae0d3d8f6cfc0ffaa70baeeeda6
devkyoung2 <devkyoung2@gmail.com>
2024-05-16 00:13:35 +0900

    [feat]: DetailBottom 데이터 적용(#158)

--- src/layouts/Product/DetailBottom/ProductItem.tsx
+++ src/layouts/Product/DetailBottom/ProductItem.tsx
@@ -1,32 +1,24 @@
-import clsx from 'clsx';
-import { Link } from 'react-router-dom';
-
 import CartButton from 'components/feature/ProductItem/CartButton';
 import Price from 'components/feature/ProductItem/Price';
-import Thumbnail from 'components/feature/ProductItem/Thumbnail';
+
+import { RecommendProductItemsResponse } from 'types/productItem';
 
 import styles from './ProductItem.module.scss';
 
-type ProductItemType = {
-  product: {
-    id: number;
-    thumbSrc: string;
-    brandName: string;
-    name: string;
-    price: number;
-    isWished: boolean;
-    wishCount: number;
-  };
-};
-
-const ProductItem = ({ product }: ProductItemType) => {
+const ProductItem = ({
+  product,
+}: {
+  product: RecommendProductItemsResponse;
+}) => {
   return (
-    <article className={clsx(styles.wrapper_prod_item)}>
-      <Link to={`/product/${product.id}`} className={styles.wrapper_main_info}>
-        <Thumbnail src={product.thumbSrc} alt={product.name} size="small" />
-        <strong className={styles.txt_prod_name}>{product.name}</strong>
-        <Price price={product.price} />
-      </Link>
+    <article className={styles.wrapper_prod_item}>
+      <img
+        src={product.photo}
+        alt={product.name}
+        className={styles.thumb_prod}
+      />
+      <strong className={styles.txt_prod_name}>{product.name}</strong>
+      <Price price={product.price} />
       <div className={styles.wrapper_util_info}>
         <CartButton />
       </div>
