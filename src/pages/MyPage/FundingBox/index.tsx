51e1b52d067c7ad5c78055ef8cea913731107698
devkyoung2 <devkyoung2@gmail.com>
2024-05-21 04:32:20 +0900

    [feat]:  펀딩함 타이틀 구현(#226)

--- src/pages/MyPage/FundingBox/index.tsx
+++ src/pages/MyPage/FundingBox/index.tsx
@@ -1,5 +1,16 @@
+import styles from './index.module.scss';
+
 const FundingBox = () => {
-  return <>펀딩</>;
+  return (
+    <>
+      <h1 className={styles.txt_title}>
+        총 n개의
+        <br />
+        펀딩을 달성했습니다. 🎉
+      </h1>
+      탭
+    </>
+  );
 };
 
 export default FundingBox;
