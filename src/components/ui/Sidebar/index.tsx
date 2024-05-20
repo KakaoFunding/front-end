d5742e43eb2d38ec855026a6023ddaa34cf40b48
devkyoung2 <devkyoung2@gmail.com>
2024-05-21 04:30:26 +0900

    [feat]: 사이드바 펀딩함 추가(#226)
    - 펀딩함을 추가하면서 생긴 주문내역과 아이콘을 변경함(pr 본문 이미지 참고)

--- src/components/ui/Sidebar/index.tsx
+++ src/components/ui/Sidebar/index.tsx
@@ -8,6 +8,7 @@ const Sidebar = () => {
       <title className={styles.txt_title}>My 선물하기</title>
       <ul className={styles.wrapper_sidebar}>
         <SidebarItem type="giftbox" titleName="선물함" />
+        <SidebarItem type="fundingbox" titleName="펀딩함" />
         <SidebarItem type="wish" titleName="위시리스트" />
         <SidebarItem type="funding" titleName="펀딩아이템" />
         <SidebarItem type="orderHistory" titleName="주문내역" />
