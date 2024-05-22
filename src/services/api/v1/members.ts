73bf17ad521a612080bc9cb9d267e9e329a1a570
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-22 23:24:07 +0900

    [refactor]: 불필요한 주석 제거 (#241)

--- src/services/api/v1/members.ts
+++ src/services/api/v1/members.ts
@@ -1,8 +1,4 @@
-import {
-  // AxiosError,
-  // AxiosRequestHeaders,
-  AxiosResponse,
-} from 'axios';
+import { AxiosResponse } from 'axios';
 
 // eslint-disable-next-line import/no-cycle
 import { apiV1 } from '.';
@@ -16,17 +12,8 @@ type RefreshAccessTokenResponseProps = {
 };
 
 export const refreshAccessToken = async (
-  // accessToken: string | null,
   refreshToken: string,
 ): Promise<AxiosResponse<RefreshAccessTokenResponseProps>> => {
-  // const headers = {
-  //   Authorization: `Bearer ${accessToken}`,
-  // };
-
-  const response = await apiV1.post(
-    '/oauth/reissue',
-    { refreshToken },
-    // { headers },
-  );
+  const response = await apiV1.post('/oauth/reissue', { refreshToken });
   return response;
 };
