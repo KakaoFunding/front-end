3190fd2a502fb69cae924d57b30d30d56343cc79
devkyoung2 <devkyoung2@gmail.com>
2024-05-23 00:44:27 +0900

    [feat]: 위시 api 연동(#258)

--- src/pages/MyPage/Wish/index.tsx
+++ src/pages/MyPage/Wish/index.tsx
@@ -1,25 +1,45 @@
+import { useEffect } from 'react';
+
 import EmptyItem from 'components/feature/EmptyItem';
+import Spinner from 'components/ui/Spinner';
+import WishItem from 'layouts/MyPage/Wish/WishItem';
+
+import { useAxios } from 'hooks/useAxios';
+
+import { WishResponse } from 'types/wish';
 
 import styles from './index.module.scss';
 
-const data = {
-  userName: '보경',
-  hasWish: true,
-};
+const userName = '보경';
 
 const Wish = () => {
+  const {
+    data: wishItems,
+    sendRequest,
+    isLoading,
+  } = useAxios<WishResponse>({
+    method: 'get',
+    url: '/wishes/me',
+  });
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
   return (
     <>
-      <div className={styles.title}>{`${data.userName}님의 \n위시리스트`}</div>
-      {data.hasWish ? (
+      {isLoading && <Spinner />}
+      <div className={styles.title}>{`${userName}님의 \n위시리스트`}</div>
+      {wishItems && (
         <ul>
-          <li>위시 1</li>
-          <li>위시 2</li>
-          <li>위시 3</li>
+          {wishItems.map((wishItem) => (
+            <li key={wishItem.productId}>
+              <WishItem wishItem={wishItem} />
+            </li>
+          ))}
         </ul>
-      ) : (
-        <EmptyItem type="wish" />
       )}
+      {wishItems || <EmptyItem type="wish" />}
     </>
   );
 };
