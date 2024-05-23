bfd5681a9355a89e757f2ccb25ed98c90afab9cb
devkyoung2 <devkyoung2@gmail.com>
2024-05-23 15:21:38 +0900

    [feat]: 주문내역 api 연동(#262)

--- src/layouts/MyPage/OrderHistory/OrderItem/index.tsx
+++ src/layouts/MyPage/OrderHistory/OrderItem/index.tsx
@@ -2,31 +2,19 @@ import { Link } from 'react-router-dom';
 
 import { formatNumberWithComma } from 'utils/format';
 
+import { OrderItemType } from 'types/order';
+
 import styles from './index.module.scss';
 
-const items = {
-  id: 1,
-  orderNumber: '662939000',
-  receiverName: '김민우',
-  product: {
-    productId: 386515,
-    name: '디핀다트 구슬아이스크림 기프트팩(50ml* 12개)',
-    photo:
-      'https://st.kakaocdn.net/product/gift/product/20220422165729_0119e39ca8a14084a3504b85ca4eaf30.jpeg',
-    price: 15900,
-    brandName: '디핀다트',
-  },
-  quantity: 1,
-  orderDate: '2024-04-24T14:59:42.48152',
-  status: 'COMPLETE_PAYMENT',
-};
-const OrderItem = () => {
+type OrderItemProps = { item: OrderItemType };
+
+const OrderItem = ({ item }: OrderItemProps) => {
   return (
-    <Link to={`/product/${items.product.productId}`}>
+    <Link to={`/product/${item.product.productId}`}>
       <div className={styles.wrapper_order}>
         <div className={styles.section_receiver}>
           <span className={styles.ico_receiver} />
-          {items.receiverName}
+          {item.receiverName}
           <span className={styles.txt_detail}>
             상세보기
             <span className={styles.ico_detail} />
@@ -34,21 +22,21 @@ const OrderItem = () => {
         </div>
         <div className={styles.section_product}>
           <img
-            src={items.product.photo}
-            alt={`${items.product.name}상품이미지`}
+            src={item.product.photo}
+            alt={`${item.product.name}상품이미지`}
             className={styles.thumb_product}
           />
           <div className={styles.info_product}>
-            <p className={styles.txt_brand}>{items.product.brandName}</p>
-            <strong className={styles.txt_title}>{items.product.name}</strong>
+            <p className={styles.txt_brand}>{item.product.brandName}</p>
+            <strong className={styles.txt_title}>{item.product.name}</strong>
             <div>
               <em className={styles.txt_price}>
-                {formatNumberWithComma(items.product.price)}
+                {formatNumberWithComma(item.product.price)}
               </em>
               원
               <span
                 className={styles.txt_quantity}
-              >{`수량${items.quantity}개`}</span>
+              >{`수량${item.quantity}개`}</span>
             </div>
           </div>
         </div>
