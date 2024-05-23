9f6377c698acee1d3f260f63027764dfb4dd43a1
devkyoung2 <devkyoung2@gmail.com>
2024-05-23 21:57:17 +0900

    [refactor]: 불필요한 axios 인스턴스 제거(#234)

--- src/services/api/v1/thema.ts
+++ src/services/api/v1/thema.ts
@@ -1,13 +1,9 @@
-import axios from 'axios';
-
 import { ProductItem } from 'types/productItem';
 
-const productApi = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
-});
+import { apiV1 } from '.';
 
 export const getThemaItems = async (categoryId: number) => {
-  const themaItem = await productApi.get(
+  const themaItem = await apiV1.get(
     `/products?size=4&categoryId=${categoryId}`,
   );
 
