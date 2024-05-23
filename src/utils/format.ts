6caac84fd6e485b7b98cdcbe81c4d1300d749cb1
devkyoung2 <devkyoung2@gmail.com>
2024-05-23 15:12:36 +0900

    [refactor]: 의미 없는 주석 제거(#278)

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -31,7 +31,7 @@ export const formatNumberToPercent = (
  */
 export const formatDate = (date: Date) => {
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth()는 0부터 시작하므로 +1 필요
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
 
   return `${year}-${month}-${day}`;
