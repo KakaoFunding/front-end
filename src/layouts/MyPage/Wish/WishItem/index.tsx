fdf09a8c7614656f267bcc100622ae78fe897e70
devkyoung2 <devkyoung2@gmail.com>
2024-05-27 13:35:27 +0900

    [feat]: 마이페이지 위시함 위시 공개여부 변경 api 연동(#283)

--- src/layouts/MyPage/Wish/WishItem/index.tsx
+++ src/layouts/MyPage/Wish/WishItem/index.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import { useState, MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useAxios } from 'hooks/useAxios';
 import { useAddWish, useDeleteWish } from 'hooks/useWish';
 import { formatNumberWithUnit } from 'utils/format';
 
@@ -12,14 +13,25 @@ import styles from './index.module.scss';
 type WishItemProp = { wishItem: WishItemType };
 
 const WishItem = ({ wishItem }: WishItemProp) => {
-  const { productId, productName, productPhoto, productPrice, isPublic } =
-    wishItem;
+  const {
+    wishId,
+    productId,
+    productName,
+    productPhoto,
+    productPrice,
+    isPublic,
+  } = wishItem;
 
   const [isWish, setIsWish] = useState<boolean>(true);
-  const [isPrivate, setIsPrivate] = useState<boolean>(isPublic);
+  const [isPrivate, setIsPrivate] = useState<boolean>(!isPublic);
 
   const { addWish } = useAddWish(productId, isPublic ? 'ME' : 'OTHERS');
   const { deleteWish } = useDeleteWish(productId);
+
+  const { sendRequest } = useAxios({
+    method: 'post',
+    url: `wishes/${wishId}/change-type`,
+  });
 
   const handleWishClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -31,9 +43,9 @@ const WishItem = ({ wishItem }: WishItemProp) => {
     setIsWish(!isWish);
   };
 
-  // TODO : API 요청
   const handleChangeVisibility = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    sendRequest();
     setIsPrivate(!isPrivate);
   };
 
@@ -58,7 +70,7 @@ const WishItem = ({ wishItem }: WishItemProp) => {
           <button type="button" onClick={handleChangeVisibility}>
             <span
               className={clsx(styles.ico_public, {
-                [styles.ico_private]: !isPublic,
+                [styles.ico_private]: isPrivate,
               })}
             >
               비밀/나만공개
