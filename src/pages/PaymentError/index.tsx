2cc9f5d0573acc7da5f02c83ef0be3efc4a67dc8
uraflower <82873315+uraflower@users.noreply.github.com>
2024-05-27 23:41:52 +0900

    [feat]: 결제 취소 페이지를 결제 오류 페이지로 변경 (#319)
    결제 취소와 실패를 결제 오류 페이지에서 모두 다룸

--- src/pages/PaymentError/index.tsx
+++ src/pages/PaymentError/index.tsx
@@ -6,12 +6,19 @@ import ErrorImage from 'assets/img_advice.png';
 
 import styles from './index.module.scss';
 
-type PaymentType = 'gift' | 'funding';
+type StateType = {
+  errorType: 'fail' | 'cancel';
+  paymentType: 'gift' | 'funding';
+  otherState: object;
+};
 
-const PaymentCancel = () => {
+const PaymentError = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
-  const { paymentType, ...otherState }: { paymentType: PaymentType } = state;
+  const { errorType, paymentType, ...otherState }: StateType = state;
+
+  const TITLE =
+    errorType === 'fail' ? '결제에 실패했습니다.' : '결제가 중단되었습니다.';
 
   const handleRetry = () => {
     const url = paymentType === 'gift' ? '/bill/gift' : '/bill/funding';
@@ -26,7 +33,7 @@ const PaymentCancel = () => {
     <article className={styles.wrapper_page}>
       <img src={ErrorImage} alt="error" className={styles.img_error} />
       <div className={styles.wrapper_txt}>
-        <strong className={styles.txt_title}>결제가 중단되었습니다.</strong>
+        <strong className={styles.txt_title}>{TITLE}</strong>
         <p className={styles.txt_desc}>
           결제를 다시 하시려면 &apos;다시 시도&apos;를 눌러주세요.
         </p>
@@ -44,4 +51,4 @@ const PaymentCancel = () => {
   );
 };
 
-export default PaymentCancel;
+export default PaymentError;
