96dfc22702aa0b904cdc80f5498809abda2f4de8
devkyoung2 <devkyoung2@gmail.com>
2024-05-27 22:26:38 +0900

    [fix]: 변경된 api 연동(#316)

--- src/layouts/Product/DetailBottom/index.tsx
+++ src/layouts/Product/DetailBottom/index.tsx
@@ -6,8 +6,6 @@ import Spinner from 'components/ui/Spinner';
 
 import { getRecommendProductItems } from 'services/api/v1/product';
 
-import { RecommendProductItemsResponse } from 'types/productItem';
-
 import ProductItem from './ProductItem';
 
 import styles from './index.module.scss';
@@ -17,31 +15,32 @@ type DetailBottomProps = {
 };
 
 const DetailBottom = ({ brandId }: DetailBottomProps) => {
-  const { data: recommendProductItem, isFetched } = useQuery({
-    queryKey: ['recommendProductItem', brandId],
-    queryFn: () => getRecommendProductItems(brandId.toString()),
+  const { data, isLoading } = useQuery({
+    queryKey: ['recommendProductItems', brandId],
+    queryFn: () => getRecommendProductItems(brandId),
   });
 
-  return isFetched ? (
-    <div>
+  return (
+    <>
       <div className={styles.wrapper_title}>
         <div className={styles.txt}>이 브랜드의 인기선물</div>
         <Link to={`/brand/${brandId}`} className={styles.button}>
           더보기<span className={styles.ico}>아이콘</span>
         </Link>
       </div>
-      <ul className={styles.wrapper_items}>
-        {recommendProductItem?.map((product: RecommendProductItemsResponse) => (
-          <li key={product.productId}>
-            <Link to={`/product/${product.productId}`} reloadDocument>
-              <ProductItem product={product} />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  ) : (
-    <Spinner />
+      {data && (
+        <ul className={styles.wrapper_items}>
+          {data.items.map((product) => (
+            <li key={product.productId}>
+              <Link to={`/product/${product.productId}`} reloadDocument>
+                <ProductItem product={product} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      {isLoading && <Spinner />}
+    </>
   );
 };
 
