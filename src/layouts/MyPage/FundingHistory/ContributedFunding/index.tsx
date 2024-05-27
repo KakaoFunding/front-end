610593dc852669053134004b8174bba4fb786316
devkyoung2 <devkyoung2@gmail.com>
2024-05-27 20:17:04 +0900

    [feat]: 기여한 펀딩내역탭 구현(#243)

--- src/layouts/MyPage/FundingHistory/ContributedFunding/index.tsx
+++ src/layouts/MyPage/FundingHistory/ContributedFunding/index.tsx
@@ -1,13 +1,66 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { useEffect, useState } from 'react';
+
+import EmptyItem from 'components/feature/EmptyItem';
 import FilterBar from 'components/ui/FilterBar';
+import Spinner from 'components/ui/Spinner';
+
+import { useDateFilter } from 'hooks/useDateFilter';
+import { useInfinityScroll } from 'hooks/useInfinityScroll';
+import { getContributedFundingHistory } from 'services/api/v1/fundingHistory';
+
+import { ContributedFundingItem } from 'types/funding';
+
+import FundingHistoryItem from '../FundingHistoryItem';
 
 import styles from './index.module.scss';
 
 const ContributedFunding = () => {
+  const [fundingItems, setFundingItems] = useState<ContributedFundingItem[]>(
+    [],
+  );
+  const [hasNext, setHasNext] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(0);
+
+  const { startDate, endDate } = useDateFilter();
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['contributedFundingHistory', startDate, endDate],
+    queryFn: () => getContributedFundingHistory(startDate, endDate),
+  });
+
+  const observingTarget = useInfinityScroll(() => {
+    if (data) setPage(data.pageNumber + 1);
+  }, hasNext);
+
+  useEffect(() => {
+    refetch();
+  }, [page]);
+
+  useEffect(() => {
+    if (data) {
+      setFundingItems(data.items);
+      setHasNext(data.hasNext);
+    }
+  }, [data]);
+
   return (
-    <section className={styles.area_funding}>
-      <FilterBar />
-      기여한 펀딩 내역
-    </section>
+    <>
+      <section className={styles.area_funding}>
+        <FilterBar />
+        <ul>
+          {fundingItems.map((item) => (
+            <li key={item.fundingDetail.fundingId}>
+              <FundingHistoryItem mode="contribute" />
+            </li>
+          ))}
+        </ul>
+        {fundingItems.length === 0 && <EmptyItem type="funding_contributed" />}
+      </section>
+      {isLoading && <Spinner />}
+      {!isLoading && hasNext && <div ref={observingTarget} />}
+    </>
   );
 };
 
