a8805c1795396e5568e15ce2643e18ae1bdba053
uraflower <82873315+uraflower@users.noreply.github.com>
2024-05-28 14:05:39 +0900

    [refactor]: 나의 위시리스트 조회 응답 타입 수정 (#306)

--- src/types/wish.ts
+++ src/types/wish.ts
@@ -1,10 +1,16 @@
-export type MyWishItemType = {
+type WishDetail = {
   wishId: number;
   productId: number;
   productName: string;
   productPrice: number;
   productPhoto: string;
-  isPublic: boolean;
+  brandName: string;
+  wishCount: number;
+};
+
+export type MyWishItemType = {
+  wishDetail: WishDetail;
+  public: boolean;
 };
 
 export type MyWishResponse = MyWishItemType[];
