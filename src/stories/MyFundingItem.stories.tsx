b63c51ceb272bbb1190651e03497799988bbb7be
devkyoung2 <devkyoung2@gmail.com>
2024-05-29 01:46:17 +0900

    [feat]: 펀딩아이템스토리북 작성(#227)

---
+++ src/stories/MyFundingItem.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import MyFundingItem from 'layouts/MyPage/FundingBox/MyFundingItem';
+
+import { MyFundingItemType } from 'types/funding';
+
+const meta: Meta<typeof MyFundingItem> = {
+  title: 'MyFundingItem',
+  component: MyFundingItem,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MyFundingItem>;
+
+const fundingItem: MyFundingItemType = {
+  id: 1,
+  product: {
+    productId: 386515,
+    name: '디핀다트 구슬아이스크림 기프트팩(50ml* 12개)',
+    photo:
+      'https://st.kakaocdn.net/product/gift/product/20220422165729_0119e39ca8a14084a3504b85ca4eaf30.jpeg',
+    price: 15900,
+    brandName: '디핀다트',
+  },
+  quantity: 1,
+  date: '2024-04-24T14:59:42.48152',
+  status: 'USABLE',
+};
+
+export const NotUsedGift: Story = {
+  args: { fundingItem },
+};
