23dbe52ce6e91fa5f3c48514425de864e7d5adf3
uraflower <82873315+uraflower@users.noreply.github.com>
2024-05-28 17:07:34 +0900

    [feat]: 나의 위시리스트 페이징 조회 적용 (#306)

--- src/pages/MyPage/Wish/index.tsx
+++ src/pages/MyPage/Wish/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 import EmptyItem from 'components/feature/EmptyItem';
 import Spinner from 'components/ui/Spinner';
@@ -13,24 +13,42 @@ import styles from './index.module.scss';
 const Wish = () => {
   const { name } = useUserStore();
 
-  const { data: wishItems, isLoading } = useQuery({
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
     queryKey: ['wishItem'],
-    queryFn: () => getMyWishItems(),
+    queryFn: ({ pageParam }) => getMyWishItems(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: ({ last, pageNumber }) => {
+      if (last) return null;
+      return pageNumber + 1;
+    },
   });
 
   return (
     <>
       {isLoading && <Spinner />}
       <div className={styles.title}>{`${name}님의 \n위시리스트`}</div>
-      {wishItems && wishItems.length === 0 && <EmptyItem type="wish" />}
-      {wishItems && wishItems.length !== 0 && (
-        <ul className={styles.wrapper_items}>
-          {wishItems.map((wishItem) => (
-            <li key={wishItem.wishDetail.wishId}>
-              <MyWishItem myWishItem={wishItem} />
-            </li>
-          ))}
+      {data?.pages.length === 0 ? (
+        <EmptyItem type="wish" />
+      ) : (
+        <ul>
+          {data?.pages.map(({ items }) =>
+            items.map((wishItem) => (
+              <li key={wishItem.wishDetail.wishId}>
+                <MyWishItem myWishItem={wishItem} />
+              </li>
+            )),
+          )}
         </ul>
+      )}
+      {hasNextPage && (
+        <button
+          type="button"
+          onClick={() => fetchNextPage()}
+          className={styles.btn_more}
+        >
+          <span className={styles.ico_more}>더보기 아이콘</span>
+          더보기
+        </button>
       )}
     </>
   );
