2a34213423103766c22004a599d7561e9f1c4c4e
devkyoung2 <devkyoung2@gmail.com>
2024-05-29 19:04:06 +0900

    [refactor]: DDay 함수 분리, 1년후 함수 로직 수정(#227)

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -2,7 +2,17 @@ export function getRandomNumber(start: number, end: number): number {
   return Math.floor(start + Math.random() * end);
 }
 
-export const getOneYearLaterDate = () => {
+export const getOneYearLaterDate = (date?: string) => {
+  if (date) {
+    const currentDate = new Date(date);
+    const oneYearLaterDate = new Date(
+      currentDate.getFullYear() + 1,
+      currentDate.getMonth(),
+      currentDate.getDate(),
+    );
+    return oneYearLaterDate;
+  }
+
   const currentDate = new Date();
   const oneYearLaterDate = new Date(
     currentDate.getFullYear() + 1,
@@ -20,4 +30,11 @@ export const getHalfYearEarlierDate = () => {
     currentDate.getDate(),
   );
   return halfYearEarlierDate;
+};
+
+export const getDDay = (date: string): number => {
+  const diffTime = new Date(date).getTime() - new Date().getTime(); // 시간 차이, 단위: ms
+  const DDay = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // 밀리초 / 1일
+
+  return DDay;
 };
