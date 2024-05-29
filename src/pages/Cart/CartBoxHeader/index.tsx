eb1f7d99a9a07d131b8f67a8db89be150f0f2c6c
ohgus <123801385+ohgus@users.noreply.github.com>
2024-05-30 01:49:22 +0900

    [feat]: 상품이 있는 상황의 카트헤더 스타일 일부 적용 (#326)

--- src/pages/Cart/CartBoxHeader/index.tsx
+++ src/pages/Cart/CartBoxHeader/index.tsx
@@ -1,13 +1,38 @@
+import clsx from 'clsx';
+
 import styles from './index.module.scss';
 
 const CartBoxHeader = () => {
+  // api 연동되면 실제 데이터로 변경될 예정
+  const isItemInCart = true;
+
   return (
     <header className={styles.header_cart}>
-      <div className={styles.area_cart_cover}>
-        <strong className={styles.ico_cart_cover}>선물상자</strong>
+      <div
+        className={clsx(styles.area_cart_cover, {
+          [styles.area_cart_cover_filled]: isItemInCart,
+        })}
+      >
+        <strong
+          className={clsx(styles.ico_cart_cover, {
+            [styles.ico_cart_cover_filled]: isItemInCart,
+          })}
+        >
+          선물상자
+        </strong>
       </div>
-      <div className={styles.area_cover_parts}>
-        <span className={styles.parts_left}>박스 왼쪽</span>
+      <div
+        className={clsx(styles.area_cover_parts, {
+          [styles.area_cover_parts_filled]: isItemInCart,
+        })}
+      >
+        <span
+          className={clsx(styles.parts_left, {
+            [styles.parts_left_filled]: isItemInCart,
+          })}
+        >
+          박스 왼쪽
+        </span>
         <span className={styles.parts_right}>박스 오른쪽</span>
       </div>
     </header>
