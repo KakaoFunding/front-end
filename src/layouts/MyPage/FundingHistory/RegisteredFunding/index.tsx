41fc60b8a334a4a803f8d6590848c492b33dceb7
devkyoung2 <devkyoung2@gmail.com>
2024-06-04 23:49:20 +0900

    [feat]: 등록한 펀딩내역탭 구현(#243)

--- src/layouts/MyPage/FundingHistory/RegisteredFunding/index.tsx
+++ src/layouts/MyPage/FundingHistory/RegisteredFunding/index.tsx
@@ -1,13 +1,68 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { useEffect, useState } from 'react';
+
+import EmptyItem from 'components/feature/EmptyItem';
 import FilterBar from 'components/ui/FilterBar';
+import Spinner from 'components/ui/Spinner';
+
+import { useDateFilter } from 'hooks/useDateFilter';
+import { useInfinityScroll } from 'hooks/useInfinityScroll';
+import { getRegisteredFundingItem } from 'services/api/v1/fundingHistory';
+
+import { RegisteredFundingItem } from 'types/fundingHistory';
+
+import FundingHistoryItem from '../FundingHistoryItem';
 
 import styles from './index.module.scss';
 
 const RegisteredFunding = () => {
+  const [fundingItems, setFundingItems] = useState<RegisteredFundingItem[]>([]);
+  const [hasNext, setHasNext] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(0);
+
+  const { startDate, endDate } = useDateFilter();
+
+  const { data, isLoading, refetch, isFetched } = useQuery({
+    queryKey: ['registeredFundingItem', startDate, endDate],
+    queryFn: () => getRegisteredFundingItem(startDate, endDate),
+  });
+
+  const observingTarget = useInfinityScroll(() => {
+    if (data) setPage(data.pageNumber + 1);
+  }, hasNext);
+
+  useEffect(() => {
+    refetch();
+  }, [page]);
+
+  useEffect(() => {
+    if (data) {
+      setFundingItems(data.items);
+      setHasNext(data.hasNext);
+    }
+  }, [data]);
+
   return (
-    <section className={styles.area_funding}>
-      <FilterBar />
-      등록한 펀딩 내역
-    </section>
+    <>
+      <section className={styles.area_funding}>
+        <FilterBar />
+        {isFetched &&
+          (fundingItems.length === 0 ? (
+            <EmptyItem type="funding_registered" />
+          ) : (
+            <ul>
+              {fundingItems.map((item) => (
+                <li key={item.fundingId}>
+                  <FundingHistoryItem mode="contribute" />
+                </li>
+              ))}
+            </ul>
+          ))}
+      </section>
+      {isLoading && <Spinner />}
+      {!isLoading && hasNext && <div ref={observingTarget} />}
+    </>
   );
 };
 
