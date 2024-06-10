90578ae40069fa8b654cdf3cc7f500cd5c1ea97b
devkyoung2 <devkyoung2@gmail.com>
2024-06-10 16:54:59 +0900

    [feat]: 선물하기 버튼 기능 연결(#202)

--- src/layouts/Cart/CartPay/index.tsx
+++ src/layouts/Cart/CartPay/index.tsx
@@ -1,7 +1,17 @@
 import clsx from 'clsx';
 import { useReducer } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from 'components/ui/Button';
+
+import { useSelectedFriendsStore } from 'store/useSelectedFriendsStore';
+
+import { useKakaoPicker } from 'hooks/useKakaoPicker';
+import { useLogin } from 'hooks/useLogin';
+
+import { RequestOrderPreview } from 'types/payment';
+
+import DefaultProfileImage from 'assets/profile_noimg.png';
 
 import BillItem from './BillItem';
 
@@ -9,9 +19,61 @@ import styles from './index.module.scss';
 
 // TODO : 가짜 상품데이터
 const prod = [1, 2, 3];
-
 const CartPay = () => {
   const [isToggled, handleToggle] = useReducer((prev) => !prev, false);
+  const navigate = useNavigate();
+  const { isLoggedIn, login, confirmLogin } = useLogin();
+  const { isSelected, isSelfSelected, selectedFriends, getImgUrl } =
+    useSelectedFriendsStore();
+  const { openKakaoPicker } = useKakaoPicker();
+
+  // 로그인 여부 확인
+  const checkLoginBeforeAction = (action: () => void) => {
+    if (isLoggedIn) action();
+    else {
+      const result = confirmLogin();
+      if (result) login();
+    }
+  };
+
+  const orderInfos: RequestOrderPreview = [
+    {
+      productId: 1361966,
+      quantity: 1,
+      options: [],
+    },
+  ];
+
+  // 나에게 선물하기 버튼 핸들러
+  const handleClickGiftForMe = () => {
+    checkLoginBeforeAction(() => {
+      navigate('/bill/gift', { state: { orderInfos, giftFor: 'me' } });
+    });
+  };
+
+  // 친구에게 선물하기 버튼 핸들러
+  const handleClickGiftForFriend = () => {
+    checkLoginBeforeAction(() => {
+      if (!isSelected) {
+        openKakaoPicker();
+        return;
+      }
+
+      if (isSelfSelected) {
+        navigate('/bill/gift', { state: { orderInfos, giftFor: 'me' } });
+      } else if (selectedFriends.length === 1) {
+        navigate('/bill/gift', { state: { orderInfos, giftFor: 'friends' } });
+      } else {
+        alert('지금은 한 번에 한 명에게만 선물할 수 있어요.');
+      }
+    });
+  };
+
+  // 친구 프로필 이미지 클릭 핸들러
+  const handleClickProfile = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    openKakaoPicker();
+  };
 
   return (
     <section className={styles.area_cart_pay}>
@@ -39,29 +101,47 @@ const CartPay = () => {
         </Button>
       </div>
 
-      <div className={styles.wrapper_buttons}>
-        <Button
-          color="black"
-          // onClick={handleClickGiftForMe}
-        >
-          나에게 선물하기
-        </Button>
-        <Button
-          color="yellow"
-          // onClick={handleClickGiftForFriend}
-        >
-          <div className={styles.wrapper_profile}>
-            {/* onClick={handleClickProfile} */}
-            <img
-              // src={getImgUrl(DefaultProfileImage)}
-              // alt="선물할 친구 프로필 사진"
-              alt="사진"
-              className={styles.img_profile}
-            />
-            <span className={styles.ico_profile} />
-          </div>
-          선물하기
-        </Button>
+      <div
+        className={clsx(styles.wrapper_buttons, {
+          [styles.is_selected]: isSelected,
+        })}
+      >
+        {!isSelected ? (
+          <>
+            <Button color="black" onClick={handleClickGiftForMe}>
+              나에게 선물하기
+            </Button>
+            <Button color="yellow" onClick={handleClickGiftForFriend}>
+              <div
+                className={styles.wrapper_profile}
+                onClick={handleClickProfile}
+              >
+                <img
+                  src={getImgUrl(DefaultProfileImage)}
+                  alt="선물할 친구 프로필 사진"
+                  className={styles.img_profile}
+                />
+                <span className={styles.ico_profile} />
+              </div>
+              선물하기
+            </Button>
+          </>
+        ) : (
+          <Button color="yellow" onClick={handleClickGiftForFriend}>
+            <div
+              className={styles.wrapper_profile}
+              onClick={handleClickProfile}
+            >
+              <img
+                src={getImgUrl(DefaultProfileImage)}
+                alt="선물할 친구 프로필 사진"
+                className={styles.img_profile}
+              />
+              <span className={styles.ico_profile} />
+            </div>
+            선물하기
+          </Button>
+        )}
       </div>
       <p className={styles.txt_cart_pay}>
         선물상자에 담은 상품은 최대 30일간 보관됩니다.
