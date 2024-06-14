9747404d77b1d5a1a273a286d6387ae89bdfbad9
devkyoung2 <devkyoung2@gmail.com>
2024-06-14 19:11:44 +0900

    [feat]: CartBox 아이템 페칭(#333)

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,4 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { useEffect, useState } from 'react';
+
 import MainWrapper from 'components/ui/MainWrapper';
+import Spinner from 'components/ui/Spinner';
 import Header from 'layouts/App/Header';
 import CartBoxFooter from 'layouts/Cart/CartBoxFooter';
 import CartBoxHeader from 'layouts/Cart/CartBoxHeader';
@@ -6,38 +11,62 @@ import CartBoxItem from 'layouts/Cart/CartBoxItem';
 import CartPay from 'layouts/Cart/CartPay';
 import EmptyCartBoxBody from 'layouts/Cart/EmptyCartBoxBody';
 
+import { getCartItems } from 'services/api/v1/cart';
+
+import { CartItem } from 'types/cart';
+
 import styles from './index.module.scss';
 
-const items = [1, 2, 3];
-
 const Cart = () => {
-  const isItemInCart = true;
+  const [selectedItems, setSelectedItems] = useState<CartItem[]>([]);
+
+  const {
+    data: cartItems,
+    isFetched,
+    isLoading,
+  } = useQuery({
+    queryKey: ['cart'],
+    queryFn: () => getCartItems(),
+  });
+
+  const isItemInCart = cartItems ? cartItems.length > 0 : false;
+
+  useEffect(() => {
+    if (cartItems) {
+      setSelectedItems([...cartItems]);
+    }
+  }, [cartItems]);
 
   return (
     <>
-      <Header />
-      <section className={styles.area_cart}>
-        <MainWrapper>
-          <div className={styles.wrapper_cart}>
-            <div className={styles.wrapper_cart_box}>
-              <CartBoxHeader isItemInCart={isItemInCart} />
-              {isItemInCart ? (
-                <ul className={styles.wrapper_item}>
-                  {items.map((it) => (
-                    <li key={it}>
-                      <CartBoxItem />
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <EmptyCartBoxBody />
-              )}
-              <CartBoxFooter isItemInCart={isItemInCart} />
-            </div>
-            <CartPay />
-          </div>
-        </MainWrapper>
-      </section>
+      {isFetched && (
+        <>
+          <Header />
+          <section className={styles.area_cart}>
+            <MainWrapper>
+              <div className={styles.wrapper_cart}>
+                <div className={styles.wrapper_cart_box}>
+                  <CartBoxHeader isItemInCart={isItemInCart} />
+                  {isItemInCart ? (
+                    <ul className={styles.wrapper_item}>
+                      {cartItems!.map((item) => (
+                        <li key={item.cartId}>
+                          <CartBoxItem />
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <EmptyCartBoxBody />
+                  )}
+                  <CartBoxFooter isItemInCart={isItemInCart} />
+                </div>
+                <CartPay />
+              </div>
+            </MainWrapper>
+          </section>
+        </>
+      )}
+      {isLoading && <Spinner />}
     </>
   );
 };
