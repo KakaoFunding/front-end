ba699010de29f68886707f7c7acd31a4bb94dc88
devkyoung2 <devkyoung2@gmail.com>
2024-06-28 15:52:36 +0900

    [feat]: 장바구니 카운트 조회 api 요청 함수작성(#385)

--- src/services/api/v1/cart.ts
+++ src/services/api/v1/cart.ts
@@ -1,4 +1,4 @@
-import { CartResponse } from 'types/cart';
+import { CartResponse, CartCountResponse } from 'types/cart';
 
 import { apiV1 } from '.';
 
@@ -6,4 +6,10 @@ export const getCartItems = async () => {
   const cartItems = await apiV1.get(`/cart`);
 
   return cartItems.data as CartResponse;
+};
+
+export const getCartCount = async () => {
+  const cartCount = await apiV1.get(`/cart/itemCount`);
+
+  return cartCount.data as CartCountResponse;
 };
